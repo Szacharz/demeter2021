bf29600d80dd19c5ead4128e9e7ff6d39b0fa7f7
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-01-20 19:06:56 +0100

    Logowanie do systemu Demeter XD

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-
+use Illuminate\Support\Facades\Session;
 class LoginController extends Controller
 {
     /*
@@ -19,10 +19,13 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers
+    {
+        logout as performLogout;
+    }
 
     /**
-     * Where to redirect users after login.
+     * Gdzie trafiają użytkownicy po zalogowaniu
      *
      * @var string
      */
@@ -36,5 +39,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    public function logout()
+    {
+       
+        Session::flush();
+        return redirect('/login');
     }
 }
