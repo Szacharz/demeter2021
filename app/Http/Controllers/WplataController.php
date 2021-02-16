7ed4fdee0cf8d991fea41bebf806f395fa52f964
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-02-16 12:29:21 +0100

    up

---
+++ app/Http/Controllers/WplataController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\wplatamodel;
+class WplataController extends Controller
+{
+    function save(Request $req)
+    {
+        $this->validate($req, [
+            'tresc'=>'required',
+            
+               ]);
+        $wplatamodel= new wplatamodel;
+        $wplatamodel->numer_wplaty=$req->numer_wplaty;
+        $wplatamodel->data=$req->data;
+        $wplatamodel->tresc=$req->tresc;
+        $wplatamodel->kwota_przychodu=$req->kwota_przychodu;
+        $wplatamodel->save();
+        return redirect('/payin');
+    }
+    function ShowData($id)
+    {
+        $wplata=wplatamodel::find($id);
+        return view('edit',['wplata'=>$wplata]);
+    }
+    function Update(Request $req)
+    {
+        $wplatamodel= wplatamodel::find($req->id);
+        $wplatamodel->numer_wplaty=$req->numer_wplaty;
+        $wplatamodel->data=$req->data;
+        $wplatamodel->tresc=$req->tresc;
+        $wplatamodel->kwota_przychodu=$req->kwota_przychodu;
+        $wplatamodel->save();
+        return redirect('/report');
+    }
+    function Delete($id)
+    {
+        $wplatamodel= wplatamodel::find($id);
+        $wplatamodel->delete();
+        return redirect('/report');
+
+    }
+   
+}
