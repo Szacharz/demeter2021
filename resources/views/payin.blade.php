bda46ab949bd1a2fcc716a4e26963f131431d5ef
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-02-18 12:28:01 +0100

    Update payin.blade.php

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -99,21 +99,11 @@
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
- 
- <script src="https://cdn.jsdelivr.net/momentjs/2.14.1/moment.min.js"></script>
-  
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  
- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/js/bootstrap-datetimepicker.min.js"></script>
-  
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.37/css/bootstrap-datetimepicker.min.css">
-  
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        
 
  <script>
 $(function () {
-  $('#today).datetimepicker();
+  $('#today').datetimepicker();
 });
 </script>
    
@@ -141,7 +131,7 @@ $(function () {
                 </div>
                 <div class="form-group">
                     <label for="data">Data wpisu:</label>
-                    <input type="date" class="form-control" id="data"  name="data" placeholder="today" >
+                    <input type="date" class="form-control" id="data"  name="data"  value=#today >
                 </div>
                 <div class="form-group">
                     <label for="deadline">Deadline</label>
