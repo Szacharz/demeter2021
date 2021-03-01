f80147786f0aeea1ee8d146782375090a14fd00c
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-01 09:33:05 +0100

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -144,6 +144,22 @@ tr:nth-child(even) {
 </form>
 </div>
 
+
+<div class="form group row">
+                    <label for="date" class="col-form-label-col-sm-2">Zakres dat od: </label>
+    <div class="col-sm-3">
+                    <input type="date" class="form-control-input-sm" id="dataod" name="dataod" required/>
+    </div>
+                    {!! "&nbsp;" !!} {!! "&nbsp;" !!}
+                    <label for="date" class="col-form-label-col-sm-2">Zakres daty do:</label>
+                    <div class="col-sm-3">
+                    <input type="date" class="form-control-input-sm" id="datado" name="datado" required/>
+                    </div>
+                    
+           <div class="col-sm-2">
+            <button type="submit"  name="search" title="search"> Wyszukaj <img src="./img/search.png" width =15, height = 15  alt="search">
+            </div>
+</div>
 <!--  poczatek szukajki 
 
    
