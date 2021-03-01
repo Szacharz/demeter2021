3c3e9c657d6eb3b9a281fa16f96e79667f7e1f24
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-01 08:11:51 +0100

    Update UsterkiController.php

--- app/Http/Controllers/UsterkiController.php
+++ app/Http/Controllers/UsterkiController.php
@@ -30,7 +30,7 @@ class UsterkiController extends Controller
         $usterki=usterkimodel::find($id_usterki);
         return view('edit',['usterki'=>$usterki]);
     }
-    function Update(Request $req)
+    function Update(Request $req, $id_usterki)
     {
         $usterkimodel= usterkimodel::find($id_usterki);
         $usterkimodel->place=$req->place;
