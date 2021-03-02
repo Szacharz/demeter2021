4214c584140935921d2d83c1f93c9228736d4181
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-02 15:29:04 +0100

    12

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -155,7 +155,7 @@ $(function () {
                         <div class="form-group">
                         <label for="autor">Status:</label>
                         <select class="form-control" name="status" id="status" >
-                        <option data-icon="fas fa-ban"></i>>Niewykonane</option>
+                        <option data-icon="fas fa-ban">Niewykonane</option>
                  </select>
     </div>
                 <button type="submit" class="btn btn-default">Dodaj usterkę</button>
