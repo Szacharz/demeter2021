9c6de46cd8f198b9dc36c439d10e9ac9be516e27
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-02 14:06:52 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,7 +85,7 @@
          <div class="row row-cols-1 row-cols-md-2">
   <div class="col mb-4">
     <div class="card">
-      <img src="./img/mine.png" class="card-img-top" alt="ph">
+      <img src="" class="card-img-top" alt="ph">
       <div class="card-body">
         <h5 class="card-title">Moje wpisy</h5>
         <p class="card-text"> Sekcja "moich wpisów". Kliknij, żeby przejść do tabeli tylko z Twoimi wpisami. Możesz dokonać edycji, usunięcia wybranego Twojego wpisu. </p>
