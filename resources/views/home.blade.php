7bf9f1a11fcaffa5e193cde4247d00c66ae86082
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 15:17:12 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,7 +84,11 @@
 
               <!-- /.card-body -->
             </div>
-
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
             <div class="container">
             <div class="column">
             <div class="col-xs-1" align="center">
