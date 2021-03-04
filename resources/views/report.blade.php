b7623fe5ed3dfc2c7970a969569fa5332ba04ac7
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 11:24:13 +0100

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -111,7 +111,7 @@
          var table = $('.yajra-datatable').DataTable({
         processing: true,
         serverSide: true,
-        ajax: "{{ route('students.list') }}",
+        ajax: "{{ route('report') }}",
         columns: [
             {data: 'usterki_id', name: 'usterki_id'},
             {data: 'tresc', name: 'tresc'},
