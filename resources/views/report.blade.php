ed734943e84ad892c3c45271bbdbb673a58dc20e
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 10:12:51 +0100

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -4,9 +4,10 @@
 <!-- Bootstrap library -->
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <script src="bootstrap/js/bootstrap.min.js"></script>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 @section('content')
 <!-- Content Header (Page header) -->
