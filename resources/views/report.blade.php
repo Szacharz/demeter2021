9921a74245c1eda584e2e7a4dc3df2c9285242c0
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 08:42:17 +0100

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -5,7 +5,8 @@
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
-
+<link href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
 @section('content')
 <!-- Content Header (Page header) -->
 
@@ -107,38 +108,6 @@
 </form>
 
 
-  <head> 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />     
-    <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-  </head>
-
-    <script type="text/javascript">
-  $(function () {
-    
-    var table = $('.data-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('report') }}",
-        columns: [
-            {data: 'id_usterki', name: 'id_usterki'},
-            {data: 'tresc', name: 'tresc'},
-            {data: 'data', name: 'data'},
-            {data: 'deadline', name: 'deadline'},
-            {data: 'autor', name: 'autor'},
-            {data: 'place', name: 'place'},
-            {data: 'status', name: 'status'},
-            {data: 'action', name: 'action', orderable: true, searchable: true},
-        ]
-      });
-    
-     });
-    </script>
 
 <table class="table table-striped table-bordered text-center table-hover table-responsive-lg data-table"> 
       <tr>
