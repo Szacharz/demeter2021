6f2d463e862871bc1a88079c8cb9bc2214d19958
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 14:53:59 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,9 +84,9 @@
 
               <!-- /.card-body -->
             </div>
-            
+
             <div class="container">
-            <div class="row">
+            <div class="column">
   <h1> Zadania "na później" </h1>
   Poniżej znajduję się tabela z zadaniami, które się "nie pala"
             <div class="table table-striped table-bordered text-center table-hover table-responsive-lg ">
