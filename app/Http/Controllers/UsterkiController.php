5a99a372a1ee16419b83bb91744109c0bb9666f9
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 08:31:08 +0100

    Update UsterkiController.php

--- app/Http/Controllers/UsterkiController.php
+++ app/Http/Controllers/UsterkiController.php
@@ -50,17 +50,5 @@ class UsterkiController extends Controller
         return redirect('/report');
 
     }
-    public function index(Request $request)
-    {
-        if ($request->ajax()) {
-            $data = Usterki::select('*');
-            return Datatables::of($data)
-                    ->addIndexColumn()
-                    ->addColumn('action'])
-                    ->rawColumns(['action'])
-                    ->make(true);
-        }
-        
-        return view('report',['usterki'=>$usterki]);
-    }
+    
 }
