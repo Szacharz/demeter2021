34f9c5814a69767a4c94b356d975221d7689bef6
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 14:20:46 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+   public function index()
     {
-        return view('home');
+        $usterki= usterkimodel::where('autor', milosz)-> toArray();
+        
+        return view('home',['usterki'=>$usterki]);
     }
+   
 }
