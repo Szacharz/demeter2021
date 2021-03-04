c13f3e4f8d16b4705f272d7bbd7c13a5067fb297
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-04 15:03:16 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,7 +91,6 @@
             <h1> Witaj na stronie głównej! </h1>
             </div>
             <div class="row"><br></br></div>
-            <div class="row"><br></br></div>
   <h2> Sprawdź zadania, które zostały odłożone na później.  </h2>
   Poniżej znajduję się tabela z zadaniami, w których deadline został wyznaczony na <b>późniejszy termin.</b> 
             <div class="table table-striped table-bordered text-center table-hover table-responsive-lg ">
