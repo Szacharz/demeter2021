ad357e734fc8a4c6adf29e0cf59702c298216649
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-05 08:58:39 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
                             <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Adres Email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autofocus>
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
