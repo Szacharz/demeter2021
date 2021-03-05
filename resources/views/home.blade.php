64cd45b01556f0aa59288c9f4587064033ead366
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-05 11:47:06 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,12 +29,14 @@
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-info">
+            <a href="payin">
               <div class="inner">
+              <div class="container">
                 <h3>Dodawanie wpisu </h3>
                 <p> Dodaj nowe zgłoszenie  </p>
               </div>
               <div class="icon">
-              
+              </div>
               </div>
               <a href="payin" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
             </div>
@@ -43,12 +45,14 @@
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-success">
+            <a href="report">
               <div class="inner">
+              <div class="container">
                 <h3>Lista Wpisów <sup style="font-size: 20px"></sup></h3>
                 <p> Podgląd wszystkich wpisów <p>
               </div>
               <div class="icon">
-                
+                </div>
               </div>
               <a href="report" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
             </div>
@@ -57,28 +61,32 @@
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-warning">
+            <a href="payout">
               <div class="inner">
+              <div class="container">
                 <h3>Placeholder </h3>
 
                 <p>Placeholder </p>
               </div>
               <div class="icon">
-         
               </div>
-              <a href="report" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+              <a href="payout" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
          <!-- ./col -->
          <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-danger">
+            <a href="#">
               <div class="inner">
+              <div class="container">
                 <h3>Placeholder</h3>
 
                 <p>Placeholder</p>
               </div>
               <div class="icon">
-              
+              </div>
               </div>
               <a href="#" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
             </div>
