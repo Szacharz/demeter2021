d5ca1b285406759f5c23b6a69a6da0847549eb4b
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-05 08:28:00 +0100

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers
+    use AuthenticatesUsers;
     {
         logout as performLogout;
     }
@@ -30,7 +30,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-
+    protected $username;
     /**
      * Create a new controller instance.
      *
@@ -39,7 +39,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->username = $this->findUsername();
     }
+    public function findUsername()
+    {
+        $login = request()->input('login');
+ 
+        $fieldType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+ 
+        request()->merge([$fieldType => $login]);
+ 
+        return $fieldType;
+    }
+    
+    public function username()
+    {
+        return $this->username;
+    }
+
     public function logout()
     {
        
