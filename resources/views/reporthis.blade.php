7a0e27f9c3109ca64e80d979c2d3c757ea3117db
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-05 15:11:27 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -157,6 +157,7 @@
         </tfoot>
     </table>
 -->
+<head>
 <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
     <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
