95b18570ad6791cd9526ec27b423780bab877049
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-15 08:52:21 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.admin')
 <head> <link rel = "icon" href = "https://www.flaticon.com/premium-icon/icons/svg/2883/2883199.svg"  type = "image/x-icon">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href=https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-
 </head>
 @section('content')
 <!-- Content Header (Page header) -->
