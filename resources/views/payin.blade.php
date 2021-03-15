f265898787d388ab6c1bb9555d2cede4c0c3e780
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-15 14:08:10 +0100

    Update payin.blade.php

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -172,7 +172,7 @@ $(function () {
     </div>
     <div class="form-group">
             <div class="form-check">
-            <input class="form-check-input" type="checkbox" value="1" id="prywatny">
+            <input class="form-check-input" type="checkbox" value="0" id="prywatny">
             <label class="form-check-label" for="prywatny">
             <b> Wpis prywatny </b> ->  Uwaga: zaznacz, gdy chcesz żeby wpis znalazł się w Twojej zakładce "Lista Prywatna."
            </label>
