e0d33def61ad02032de85072234da76a7240740d
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-16 11:43:04 +0100

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -102,9 +102,20 @@
          
           <script>
 $(document).ready(function() {
-  $('#usterki').DataTable( {
-        "lengthMenu": [[-1, 10, 25, 50], ["All", 10, 25, 50, ]]
+    var t = $('#usterki').DataTable( {
+        "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+        "order": [[ 1, 'asc' ]]
     } );
+ 
+    t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
 } );
 </script>
 
