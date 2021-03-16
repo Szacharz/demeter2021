1e6089b564682a95abbb2507eaabcabdeaef699e
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-16 11:20:14 +0100

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -102,7 +102,7 @@
          
           <script>
 $(document).ready(function() {
-  var t = $('#usterki').DataTable( {
+  $('#usterki').DataTable( {
         "lengthMenu": [[-1, 10, 25, 50], ["All", 10, 25, 50, ]]
     } );
         "columnDefs": [ {
