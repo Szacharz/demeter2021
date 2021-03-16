77c54e6c3d03aa7952ab597f24348fb097c55ac9
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-16 15:46:17 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 <script  src="https://code.jquery.com/jquery-3.5.1.js"> </script>
 <script  src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"> </script>
 <script  src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"> </script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
 <script>
 $(document).ready(function() {
     $('#example').DataTable( {
