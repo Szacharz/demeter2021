b2d1a3141a99d8574a8b410ad1547eb10f8eda88
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-16 12:58:33 +0100

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -192,12 +192,17 @@
     @yield('content')
   </div>
   <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    
+    <div class="float-right d-none d-sm-inline-block">
+    Dmeter HelpDesk-By @Miłosz Walkowski
+    </div>
+  </footer>
+
   <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
+
   <!-- /.control-sidebar -->
-</div>
+
 <!-- ./wrapper -->
 
 <!-- jQuery -->
