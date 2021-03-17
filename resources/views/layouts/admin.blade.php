d088f5d4181d492df05dbdd3e463a635d6658e41
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-17 09:33:07 +0100

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -84,10 +84,6 @@
                                 </li>
                             @endif
 	</span>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="false" href="#"><i
-            class="fa fa-th-large"></i></a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
