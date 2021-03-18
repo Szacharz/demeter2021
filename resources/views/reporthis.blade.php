c7742ef3e6dd9b140138d3281b49c3c3b70822cd
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-18 08:48:38 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -102,7 +102,7 @@
          
           <script>
 $(document).ready(function() {
-    var t = $('#example').DataTable( {
+    var table = $('#example').DataTable( {
         "columnDefs": [ {
             "searchable": false,
             "orderable": false,
@@ -113,13 +113,13 @@ $(document).ready(function() {
         
     } );
  
-    t.on( 'order.dt search.dt', function () {
+    table.on( 'order.dt search.dt', function () {
         t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
             cell.innerHTML = i+1;
         } );
     } ).draw();
 
-    t.on('click', 'td', function () {
+    table.on('click', 'td', function () {
         var name = $('td', this).eq(1).text();
         $('#Modal').modal("show");
     });
