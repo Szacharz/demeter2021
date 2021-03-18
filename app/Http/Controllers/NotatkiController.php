229d63cb9f647b25e47cd082334000467b9656a9
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-18 14:33:54 +0100

    Update NotatkiController.php

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Notatki;
+use App\Models\usterkimodel;
 use Illuminate\Http\Request;
 
 class NotatkiController extends Controller
 {
-    //
+    function save(Request $req)
+    {
+	$this->validate($req, [
+	    'tresc_nt'=>'required',
+	    'autor'=>'required',
+	]);
+    $usterkimodel= usterkimodel::find($req->input('id_usterki'));
+	$Notatki= new Notatki;
+	$Notatki->tresc_nt=$req->tresc_nt;
+	$Notatki->autor=$req->autor;
+	$Notatki->save();
+	return redirect('/reporthis');
+    }
 }
