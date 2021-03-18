f3c5a39fb0dba47d9c1dda91463a6ebf86a85322
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-18 09:47:32 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -106,8 +106,8 @@ $(document).ready(function() {
         "columnDefs": [ {
             "searchable": false,
             "orderable": false,
-            "targets": 0
-            "render": function(data, type, row) {return '<button class="btn btn-primary" data-toggle="modal" data-id="'+row.id+'" data-fieldname="'+row.fieldname+'" data-target="#myModal">'+data+'</button>'} }
+            "targets": 0,
+            "render": function(data, type, row) {return '<button class="btn btn-primary" data-toggle="modal" data-id="'+row.id+'" data-fieldname="'+row.fieldname+'" data-target="#myModal">'+data+'</button>'} 
         } ],
         "lengthMenu": [[-1, 10, 25, 50], ["All", 10, 25, 50, ]],
         "order": [[ 1, 'asc' ]],
