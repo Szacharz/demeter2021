7e5374cd6a3ac4e7772b9786186c79e928c9b524
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-18 09:09:16 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -124,7 +124,7 @@ $(document).ready(function() {
     
     $('#example').on('click', 'td', function () {
         var name = $('td', this).eq(1).text();
-        $('#Modal').modal("show");
+        $('#Modal-id').modal("show");
     });
 
 } );
