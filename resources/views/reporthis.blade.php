dc0bd8687c96d9d26538c851455fdeecc7241ea1
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-18 10:33:21 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -173,8 +173,6 @@ $(document).ready(function() {
   <!-- Modal content-->
   <div class="modal-content">
   <form class="form-example" action="/edit" method="POST">
-            @csrf
-            <input type="hidden" name="id_usterki" id="id_usterki" value="{{$usterki['id_usterki']}}">
          
                 <h1>Edycja Wpisu o ID: {{$usterki['id_usterki']}}</h1>
                 <p class="description">Dodaj nowy Wpis, wypełniajac formularz.</p>
