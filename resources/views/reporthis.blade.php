c26712d32fc9446416df11afcc53b9ed1a08616e
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-18 15:02:55 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -124,7 +124,7 @@ $(document).ready(function() {
     t.on('click', 'td', function () {
       
         var tresc =$(this).closest('tr').find('td:eq(2)').html();
-      $('#editForm').attr('action', '/notatki/')
+      $('#editForm').attr('action', '/save/')
        window.$('#modal-id').modal("show");
        $(".ptresc").html("src","");
        $(".ptresc").html("<b> 1. Treść wpisu:     </b>"+tresc);
