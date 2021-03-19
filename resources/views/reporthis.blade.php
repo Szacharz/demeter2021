64e938bfa88678cedb600de0d9e504837e7ce835
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-19 08:34:48 +0100

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -183,7 +183,7 @@ $(document).ready(function() {
     </div>
 
 
-    <form  id="addNote" action="{{ action('NotatkiController@save') }} method="POST">
+    <form  id="addNote" action="{{ action('NotatkiController@save') }}" method="POST">
             {{ csrf_field() }}
 
 
