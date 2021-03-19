393621c55841e55dd39f6dbb5406b6754c4c73e8
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-19 15:10:08 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,12 +23,16 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         
     ];
-    public function render($request, Exception $exception)
-    
-     { 
-         if ($exception instanceof MethodNotAllowedHttpException) { abort(404); } 
-         return parent::render($request, $exception); 
+    public function render($request, Exception $e)
+    {
+        if ($e instanceof TokenMismatchException){
+            // Redirect to a form. Here is an example of how I handle mine
+            return redirect($request->fullUrl())->with('csrf_error',"Oops! Seems you couldn't submit form for a long time. Please try again.");
         }
+
+        return parent::render($request, $e);
+    }
+}
     /**
      * The application's route middleware groups.
      *
