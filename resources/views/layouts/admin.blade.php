114d8c2921b9b6396535f47acfc5dffa3525f17a
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-29 11:22:05 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -44,7 +44,7 @@
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+  <nav class="main-header navbar navbar-expand bg-white navbar-secondary border-bottom">
     <!-- Left navbar links -->
 	
     <ul class="navbar-nav">
