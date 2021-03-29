52685ffcaafc3f389abcd9c487996d43413533d5
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-29 14:34:51 +0200

    Update NotatkiController.php

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -8,25 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class NotatkiController extends Controller
 {
-    function save(Request $req)
-    {
-	$this->validate($req, [
-	    'tresc_nt'=>'required',
-	]);
-
-	$user_name=Auth::user()->name;
-    $usterkimodel= usterkimodel::find($req->input('id_usterki'));
-	$Notatki= new Notatki;
-	$Notatki->tresc_nt=$req->tresc_nt;
-	$Notatki->autor=$req->$user_name;
-	$Notatki->id_usterki=$req->id_usterki;  
-	$Notatki=Notatki::where('id_usterki', $id_usterki)->update(array('autor'=> $user_name)); 
-	$Notatki->save();
-	return redirect('/reporthis')->with('success', 'Dodano Notatkę');
-    }
-
-
-
 	function appearData($id_usterki)
     {
         $usterki=usterkimodel::find($id_usterki);
