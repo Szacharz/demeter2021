4f7a912f0cb888246f2e630e7310e21f1509994e
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-29 14:52:26 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::post('/notatkisubmit','App\Http\Controllers\NotatkiController@save');
 
 Route::get('note/{id_usterki}','App\Http\Controllers\NotatkiController@appearData');
 
-Route::get('note','App\Http\Controllers\NotatkiController@note');
+Route::post('note','App\Http\Controllers\NotatkiController@note');
 
 
 ?>
