603107410f0463aa2dfc4b52f9c39a23c87c9581
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-29 14:27:02 +0200

    12

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -34,15 +34,13 @@ class NotatkiController extends Controller
     }
 
 	function note (Request $req)
-    {
+    
+    {   $user_name=Auth::user()->name;
         $usterkimodel= usterkimodel::find($req->input('id_usterki'));
-        $usterkimodel->place=$req->place;
-        $usterkimodel->data=$req->data;
-        $usterkimodel->tresc=$req->tresc;
-        $usterkimodel->autor=$req->autor;
-		$usterkimodel->deadline=$req->deadline;
-		$usterkimodel->status=$req->status;
-        $usterkimodel->save();
+        $Notatki->tresc_nt=$req->tresc_nt;
+        $Notatki->id_usterki=$req->id_usterki;
+        $Notatki=Notatki::where('id_usterki', $id_usterki)->update(array('autor'=> $user_name)); 
+        $Notatki->save();
         return redirect('/report');
     }
 }
