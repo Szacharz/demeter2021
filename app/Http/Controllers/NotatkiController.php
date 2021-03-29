ad25405358d42d8828d669c61400f37e8335321f
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-29 10:36:19 +0200

    Update NotatkiController.php

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -19,8 +19,8 @@ class NotatkiController extends Controller
 	$Notatki= new Notatki;
 	$Notatki->tresc_nt=$req->tresc_nt;
 	$Notatki->autor=$req->$user_name;
-	//* $Notatki->id_usterki=$req->id_usterki;  */
-	//* $Notatki=Notatki::where('id_usterki', $id_usterki)->update(array('autor'=> $user_name)); */
+	$Notatki->id_usterki=$req->id_usterki;  */
+	 $Notatki=Notatki::where('id_usterki', $id_usterki)->update(array('autor'=> $user_name)); */
 	$Notatki->save();
 	return redirect('/reporthis')->with('success', 'Dodano Notatkę');
     }
