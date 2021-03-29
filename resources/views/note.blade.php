b7e8d8aa766b5e91269608c35da3d95881505277
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-29 14:08:19 +0200

    Update note.blade.php

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -6,136 +6,166 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Demeter</h1>
+            <h1 class="m-0 text-dark">Dementor. System wpisów-zadań.</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Strona główna</a></li>
+              <!-- dodanie po prawej stronie w headzie, nie main headzie -->
+              <ol class="breadcrumb float-sm-right">
+          
               
-            </ol>
+          </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
-    <title>Dementor</title>
-    <head><link rel = "icon" href = "https://www.vhv.rs/dpng/d/539-5398503_transparent-dementor-png-silhouette-harry-potter-dementor-png.png"  type = "image/x-icon">
-</head>
+
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
+      <a href="http://dementor/payin">
         <!-- Small boxes (Stat box) -->
         <div class="row">
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>Dodawanie wpłaty </h3>
-                <p> Dodaj nowy przychód </p>
+                <h3>Dodawanie wpisu</h3>
+                <p> Dodaj nowe zgłoszenie</p>
               </div>
               <div class="icon">
               
               </div>
-              <a href="payin" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
+              <a href="http://dementor/payin" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-success">
+            <a href="http://dementor/report">
               <div class="inner">
-                <h3>Dodaj wypłatę <sup style="font-size: 20px"></sup></h3>
-                <p> Dodaj nowy rozchód <p>
+                <h3>Lista zgłoszeń <sup style="font-size: 20px"></sup></h3>
+                <p> Podgląd i edycja listy zgłoszeń <p>
               </div>
               <div class="icon">
                 
               </div>
-              <a href="payout" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
+              <a href="http://dementor/report" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-warning">
+            <a href="http://dementor/payout">
               <div class="inner">
-                <h3>Raport miesięczny</h3>
+                <h3>Lista Prywatna</h3>
 
-                <p>Kliknij, by utworzyć Raport miesięczny</p>
+                <p>Podgląd prywatnych zadań</p>
               </div>
               <div class="icon">
          
               </div>
-              <a href="report" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
+              <a href="http://dementor/reporthis" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
          <!-- ./col -->
          <div class="col-lg-3 col-6">
             <!-- small box -->
             <div class="small-box bg-danger">
+            <a href="reporthis">
               <div class="inner">
-                <h3>Historia Raportów</h3>
+              <div class="container">
+                <h3>Archiwum</h3>
 
-                <p>Kliknij, aby przejrzeć historię raportów</p>
+                <p>Podgląd archiwalnych wpisów</p>
               </div>
               <div class="icon">
-              
               </div>
-              <a href="reporthis" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+              <a href="http://dementor/reporthis"" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
             </div>
           </div>
                
-          <div id="container">
-    <h2> Edycja wypłaty (rozchodu) </h2>
-    @if(count($errors)>0)
-    <ul>
+          <div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-10 col-md-8 col-lg-6">
+            <!-- Form -->
+            @if(count($errors)>0)
+           <ul>
       @foreach($errors->all() as $error)
     <li class="alert alert-danger">{{$error}}</li>
     @endforeach
     </ul>
     @endif
-   <form class="form-horizontal" action="/edit2" method="POST">
-   @csrf
+            <form class="form-example" action="/edit" method="POST">
+            @csrf
+            <input type="hidden" name="id_usterki" id="id_usterki" value="{{$usterki['id_usterki']}}">
+         
+                <h1>Edycja Wpisu o ID: {{$usterki['id_usterki']}}</h1>
+                <p class="description">Dodaj nowy Wpis, wypełniajac formularz.</p>
+                <!-- Input fields -->
+                <div class="form-group">
+                    <label for="tresc">Opis wpisu:</label>
+                    <input type="text" class="form-control" id="tresc" placeholder="Podaj treść wpisu..." name="tresc" value="{{$usterki['tresc']}}">
+                </div>
+                <div class="form-group">
+                    <label for="data">Data wpisu:</label>
+                    <input type="date" class="form-control" id="data"  name="data"  value="{{$usterki['data']}}" >
+                </div>
+                <div class="form-group">
+                    <label for="deadline">Deadline</label>
+                    <select class="form-control" name="deadline" id="deadline">
+                    <option>Dziś (
+                      <?php echo date('d-m-Y'); ?> 
+                    )
+                    </option>
+                    <option>Jutro (
+                    <?php 
+                    $datetime = new DateTime('tomorrow');
+                    echo $datetime->format('d-m-Y');
+                    ?> 
+                    )
+                    </option>
+                    <option>Ten Tydzień (Do: 
+                      <?php
+                      date_default_timezone_set('Europe/Warsaw'); //this is the default in php.ini
 
-   <input type="hidden" name="id" value="{{$wyplata['id']}}">
-   <div class="form-inline">
-   <div class="form-group">
-  
-   {!! "&nbsp;" !!} {!! "&nbsp;" !!} {!! "&nbsp;" !!} 
-  <label class="control-label col-sm-2" for="data">Numer wpłaty:
-   <div class="col-sm-10">
-   <input type="number" name="numer_wyplaty" class="form-control" id="numer_wyplaty" value="{{$wyplata['numer_wyplaty']}}"> </label>
-  </div>
-</div>
-
-<div class="center">
-   <label class="control-label col-sm-2" for="data">Data:
-   <div class="col-sm-10">
-   <input type="date" name="data" class="form-control" id="data" value="{{$wyplata['data']}}"> </label>
-  </div>
-</div>
-
-
-<div class="form-group">
-<label class="control-label col-sm-2" for="tresc">Opis:
-<div class="col-sm-10"> 
-   <input type="text" action="#" name="tresc" class="form-control" placeholder="Wprowadź opis wypłaty" id="tresc" value="{{$wyplata['tresc']}}"> </label><!-- drugi wiersz 2 kolumna -->
-   </div>
+                      $monday = strtotime('monday this week');
+                      $sunday = strtotime('sunday this week');
+                      echo $this_week_ed = date("d-m-Y",$sunday)."<br>";
+                      ?>
+                      )
+                    </option>
+                    <option>Później</option>
+                    </select>
+                    </div>
+                <div class="form-group">
+                    <label for="place">Miejsce zdarzenia (Opcjonalne):</label>
+                    <input type="text" class="form-control" id="place" placeholder="Wpisz miejsce zdarzenia..." name="place" value="{{$usterki['place']}}">
+                </div>
+                <div class="form-group">
+                    <label for="autor"></label>
+                    <input type="hidden" class="form-control"   name="autor" id="autor" value="{{$usterki['autor']}}" >
+            
+                        <label for="autor">Status:</label>
+                        <select class="form-control text-danger" name="status" id="status">
+                   <option>Wykonane</option>
+                  <option>W trakcie</option>
+                  <option>Niewykonane</option>
+                     </select>
     </div>
-
- 
-    <div class="form-group">
-<label class="control-label col-sm-2" for="tresc">Kwota wypłaty:   
-<div class="col-sm-10"> 
-   <input type="number" class="form-control" placeholder="1.00" step="0.01" min="0" max="100000000" name="kwota_rozchodu" value="{{$wyplata['kwota_rozchodu']}}"></label> 
-   </div>
+                
+                <button type="submit" class="btn btn-default">Edytuj wpis</button>
+                <!-- End input fields -->
+                </form>
+                </div>
+            </form>
+            <!-- Form end -->
+        </div>
     </div>
-
-
-   <button type="submit" class="btn btn-default">123</button>
- 
-              
-
-           
+</div>
            
 
             
