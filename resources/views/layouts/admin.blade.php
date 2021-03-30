0d7117b6aa0197ccbd0bbe723ca8e308c22627e3
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-30 09:36:48 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -101,9 +101,7 @@
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-      <div class="image">
       <i class="fas fa-user"></i>
-      </div>
       <div class="info">
       <a href='#' class="d-block">
     
