32586db56b9d306748feee61cbc02e57335ef18b
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-30 09:03:55 +0200

    Update payout.blade.php

--- resources/views/payout.blade.php
+++ resources/views/payout.blade.php
@@ -91,8 +91,15 @@
             </div>
           </div>
 
-          @if (!empty($success))
-    {{ $success }}
+          @if (session('success'))
+    <div class="col-sm-12">
+        <div class="alert  alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+        </div>
+    </div>
 @endif
 
           <script>
