e13e9320af05195bae147b23cc70d02f5050192f
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-30 09:35:11 +0200

    12

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -31,6 +31,6 @@ class NotatkiController extends Controller
         $Notatki->id_usterki=$req->id_usterki;
         $Notatki->autor=$req->autor;
         $Notatki->save();
-        return redirect('payout')->with('success', 'Dodano Notatkę');
+        return redirect('payout')->with('success', 'Pomyślnie dodano nową notatkę do wpisu!');
     }
 }
