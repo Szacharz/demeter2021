71a6cb1febbc8f927a2fcad9caca8f6fad5bdf3d
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-03-31 08:31:55 +0200

    Revert "1"
    This reverts commit 918e1d64aa1924b22f7a1ec948c5cdf6844e8e2a.

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -108,18 +108,7 @@
        Format: 'dd/mm/yyyy' 
    });
 </script>
-
-
-@if (session('success'))
-    <div class="col-sm-12">
-        <div class="alert  alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-        </div>
-    </div>
-@endif
+   
     
 
    <div class="container">
