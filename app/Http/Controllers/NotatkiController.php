81b2c7c8637c6ccd4c4a704ab77d9854d64dc962
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-01 13:08:27 +0200

    Update NotatkiController.php

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -24,6 +24,7 @@ class NotatkiController extends Controller
         ]);
 
         $user_name=Auth::user()->name;
+        $usterkimodel= usterkimodel::find($req->input('id_usterki'));
         $Notatki=new Notatki;
         $Notatki->tresc_nt=$req->tresc_nt;
         $Notatki->id_usterki=$req->id_usterki;
