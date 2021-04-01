b3ae2ea540e2c9ca90b44525dcb4758cd1e0acff
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-01 13:34:16 +0200

    Update NotatkiController.php

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -24,12 +24,12 @@ class NotatkiController extends Controller
         ]);
 
         $user_name=Auth::user()->name;
-        $usterkimodel= usterkimodel::find($req->input('id_usterki'));
+        $usterkimodel= usterkimodel::find($id_usterki));
         $Notatki=new Notatki;
         $Notatki->tresc_nt=$req->tresc_nt;
         $Notatki->id_usterki=$req->id_usterki;
         $Notatki->autor=$req->autor;
-        $usterki=usterkimodel::where($id_usterki, 'id_usterki')->update(array('notki'=> "Tak"));
+        $usterki=usterkimodel::where($id_usterki)->update(array('notki'=> "Tak"));
         $Notatki->save();
         return redirect('report')->with('success', 'Pomyślnie dodano nową notatkę do wpisu!');;
     }
