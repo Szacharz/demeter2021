c85bd80dda2b0534614a7c8171dfed759342c65f
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-12 12:37:32 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -136,7 +136,7 @@ $(document).ready(function()
   </div>
 
 <div class="row">
-<table class="table table-striped table-bordered text-center table-hover table-responsive-lg" cellspacing="0" style="width:100%"  id="later">
+<table class="table table-striped table-bordered text-center table-fixed table-hover table-responsive-lg" cellspacing="0" style="width:100%"  id="later">
      <thead>
       <tr>
       <th scope="col">Id </th>
