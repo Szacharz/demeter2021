7cbc1c48403f064481e4148cce7f7b624ba91bf4
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-12 12:34:03 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -135,7 +135,7 @@ $(document).ready(function()
   *Poniżej znajduję się tabela z zadaniami <b>Niewykonanymi</b>, lub <b>W trakcie</b>, w których deadline został wyznaczony na <b>późniejszy termin.</b> 
   </div>
 
-
+<div class="row">
 <table class="table table-striped table-bordered text-center table-hover table-responsive-lg" cellspacing="0" style="width:100%"  id="later">
      <thead>
       <tr>
@@ -163,6 +163,7 @@ $(document).ready(function()
       @endforeach
       </tbody>
     </table>
+</div>
               <!-- /.card-body -->
             </div>
             <!-- /.card -->
