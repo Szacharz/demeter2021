033fbf5551309d21f91a15a4440c3e6e69aeff2f
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-13 08:25:54 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,9 +57,10 @@ $(document).ready(function()
             <div class="container">
             
             <div class="column">
-            <div class="col-xs-1" align="center">
+            
            <p class="text-info"> 13-04-2021 1. Dodano możliwość zmiany hasła w panelu bocznym po kliknięciu na nazwę użytkownika. </p>
-           <p class="text-info"> 13-04-2021 2. Zmieniono ogólny wygląd strony. </p>
+           <p class="text-info">  2. Zmieniono ogólny wygląd strony. </p><
+            <div class="col-xs-1" align="center">
             <h1> Witaj na stronie głównej! </h1>
   <h2> Sprawdź zadania, które zostały odłożone na później*.  </h2>
   </div>
