6d4864b1e761e14995d97a77dd9bda8b6df980b3
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-13 14:35:51 +0200

    Update ExpirationController.php

--- app/Http/Controllers/ExpirationController.php
+++ app/Http/Controllers/ExpirationController.php
@@ -24,7 +24,7 @@ class ExpirationController extends Controller
         $todayDate = Carbon::now()->format('Y-m-d');
         $usterki = usterkimodel::where('deadline','<',$todayDate)
         ->where('status', "Niewykonane")
-        ->orWhere('status', "W trakcie")
+        ->where('status', "W trakcie")
         ->get();
         return view('expiration',['usterki'=>$usterki]);
     }
