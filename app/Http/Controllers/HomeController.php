845469361ba7e483d9eee1fafd9d902779b05ab1
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-13 14:18:57 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     {
         $usterkilate=usterkimodel::where('deadline', "Później")
         ->where('status', "Niewykonane")
-        ->where('status', "W trakcie")
+        ->orWhere('status', "W trakcie")
         ->get();
         return view('home',['usterkilate'=>$usterkilate]);
     }
