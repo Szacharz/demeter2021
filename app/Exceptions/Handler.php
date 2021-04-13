fbb34bbf32c1bde2931ab9f6d4325232c0ffc9cd
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-13 13:24:27 +0200

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,5 +38,10 @@ class Handler extends ExceptionHandler
             //
         });
     }
-    
+    public function render($request, Exception $exception){
+        if ($exception instanceof AuthenticationException) {
+            return redirect('/login');
+        }
+        return parent::render($request, $exception);
+    }
 }
