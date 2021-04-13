6869a80b2a767928bbf873e610650a3dfee772b6
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-13 07:47:00 +0200

    Update reporthis.blade.php

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -1,103 +1,10 @@
 @extends('layouts.admin')
 
-
-<!-- Bootstrap library -->
-
-
 @section('content')
 <!-- Content Header (Page header) -->
-
-
-
-<div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Dementor. System wpisów-zadań.</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-              <!-- dodanie po prawej stronie w headzie, nie main headzie -->
-              <ol class="breadcrumb float-sm-right">
-          
-              
-          </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-            <a href="payin">
-              <div class="inner">
-              <div class="container">
-                <h3>Dodawanie wpisu </h3>
-                <p> Dodaj nowe zgłoszenie  </p>
-              </div>
-              <div class="icon">
-              </div>
-              </div>
-              <a href="payin" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-            <a href="report">
-              <div class="inner">
-              <div class="container">
-                <h3>Lista zgłoszeń <sup style="font-size: 20px"></sup></h3>
-                <p> Podgląd wszystkich zgłoszeń <p>
-              </div>
-              <div class="icon">
-                </div>
-              </div>
-              <a href="report" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-            <a href="payout">
-              <div class="inner">
-              <div class="container">
-                <h3>Lista Prywatna </h3>
-
-                <p>Podgląd prywatnych zadań </p>
-              </div>
-              <div class="icon">
-              </div>
-              </div>
-              <a href="payout" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-         <!-- ./col -->
-         <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-            <a href="reporthis">
-              <div class="inner">
-              <div class="container">
-                <h3>Archiwum</h3>
-
-                <p>Podgląd archiwalnych wpisów</p>
-              </div>
-              <div class="icon">
-              </div>
-              </div>
-              <a href="reporthis" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-
+      
                
          
           <script>
