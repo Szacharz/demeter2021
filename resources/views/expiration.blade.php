fac48b0186c5ee91f1784b8435d38e838abfabc3
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-14 13:16:42 +0200

    Update expiration.blade.php

--- resources/views/expiration.blade.php
+++ resources/views/expiration.blade.php
@@ -2,13 +2,12 @@
 
 
 <!-- Bootstrap library -->
-
-
-@section('content')
-
 <head>
 <link rel = "icon" href = "https://www.vhv.rs/dpng/d/539-5398503_transparent-dementor-png-silhouette-harry-potter-dementor-png.png"  type = "image/x-icon">
 </head>
+
+@section('content')
+
 
     <!-- Main content -->
     <section class="content">
