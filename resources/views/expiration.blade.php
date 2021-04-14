562e43c80239b428db2b0b7b85421498ea0baa26
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-14 13:16:05 +0200

    12

--- resources/views/expiration.blade.php
+++ resources/views/expiration.blade.php
@@ -6,7 +6,9 @@
 
 @section('content')
 
-
+<head>
+<link rel = "icon" href = "https://www.vhv.rs/dpng/d/539-5398503_transparent-dementor-png-silhouette-harry-potter-dementor-png.png"  type = "image/x-icon">
+</head>
 
     <!-- Main content -->
     <section class="content">
