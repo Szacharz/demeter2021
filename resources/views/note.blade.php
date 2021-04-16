b887f96a36e88b2d17bf9b2eba42158736445809
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-16 08:10:52 +0200

    Update note.blade.php

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -49,7 +49,7 @@
             
             <form class="form-inline">
           <div class="form-group mb-2">
-           <div class="card-header cell-breakWord"><h1>Tytuł wpisu: </h1> <h4>{{$usterki['tresc']}}</h4> </div>
+           <div class="card-header cell-breakWord"><h1>Tytuł wpisu: </h1> <h5><b>{{$usterki['tresc']}}</b></h5> </div>
            </div>
             <div class="card-body">
             <div class="form-group">
