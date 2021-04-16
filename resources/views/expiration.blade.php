cbbb58cb6ccb5e639d05d36650319a92c301d3d9
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-16 07:37:48 +0200

    Update expiration.blade.php

--- resources/views/expiration.blade.php
+++ resources/views/expiration.blade.php
@@ -138,7 +138,7 @@ $(document).ready(function() {
 </form>
 
 
-<table class="table table-striped table-bordered text-center table-hover table-responsive-lg" style="width:100%" id="usterki"> 
+<table class="table table-striped table-bordered text-center table-hover table-responsive-lg"  id="usterki"> 
      <thead>
       <tr>
       <th>LP</th>
