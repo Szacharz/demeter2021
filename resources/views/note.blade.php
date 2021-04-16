8a4f8aaf5317aa6ccd9ec0109152a241e770dcb4
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-16 08:48:15 +0200

    Update note.blade.php

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.admin')
 
+<head>
+<link rel = "icon" href = "https://www.vhv.rs/dpng/d/539-5398503_transparent-dementor-png-silhouette-harry-potter-dementor-png.png"  type = "image/x-icon">
+</head>
+
+
 @section('content')
 <!-- Content Header (Page header) -->
     <!-- Main content -->
