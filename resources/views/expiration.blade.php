3017a03785594842802f0cb84bbf04ccbdb24adb
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-16 07:22:11 +0200

    Update expiration.blade.php

--- resources/views/expiration.blade.php
+++ resources/views/expiration.blade.php
@@ -153,8 +153,8 @@ $(document).ready(function() {
    </div>  
       <tr>
         <td></td>
-        <td class="white-space: wrap;"><a href={{"note/".$row['id_usterki']}}>{{$row['data']}}</td>
-        <td class="white-space: wrap;"><a href={{"note/".$row['id_usterki']}}>{{$row['tresc']}}</td>
+        <td><a href={{"note/".$row['id_usterki']}}>{{$row['data']}}</td>
+        <td><a href={{"note/".$row['id_usterki']}}>{{$row['tresc']}}</td>
         <td><a href={{"note/".$row['id_usterki']}}>{{$row['deadline']}}</td>
         <td><a href={{"note/".$row['id_usterki']}}>{{$row['autor']}}</td>
         <td><a href={{"note/".$row['id_usterki']}}>{{$row['place']}}</td>
