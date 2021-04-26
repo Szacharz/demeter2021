c44b792036b5e88ac47c5cd855c606405d9da371
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-26 11:09:22 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -58,10 +58,12 @@
   <a class="btn btn-danger " href='http://dementor/expiration'role="button"><h6><i class="fa fa-gavel"></i> Przedawnione wpisy</h6></a>
   <a class="btn btn-warning " href='http://dementor/payout'role="button"><h6><i class="fa fa-lock"></i> Lista prywatna</h6> </a>
   <a class="btn btn-info " href='http://dementor/reporthis'role="button"><h6><i class="fa fa-archive"></i> Archiwum </h6></a>
-  
- 
+  <?php 
+  $user_name=Auth::user()->role;
+  if (role=="Admin"){
     <a class="btn btn-info" href='http://dementor/manage'role="button"><h6><i class="fa fa-shield"></i> Panel Administracyjny </h6></a>
-  
+  }
+  ?>
 
 </div>
       </li>
