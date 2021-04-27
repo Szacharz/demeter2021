e1223cc49940f03a5cbd7639eb52e30d4dc6713c
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-27 12:03:09 +0200

    Update newuser.blade.php

--- resources/views/newuser.blade.php
+++ resources/views/newuser.blade.php
@@ -44,7 +44,6 @@ a
         </div>
                 <div class="card-body">
                     <form method="POST" action="/register">
-                        @csrf
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nazwa') }}</label>
