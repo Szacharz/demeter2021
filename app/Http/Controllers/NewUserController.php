9d0323be10a5fbd7e4f4424d00d80afe878f682e
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-27 10:46:09 +0200

    Update NewUserController.php

--- app/Http/Controllers/NewUserController.php
+++ app/Http/Controllers/NewUserController.php
@@ -14,40 +14,6 @@ use Illuminate\Http\Request;
 
 class NewUserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    use RegistersUsers;
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('newuser');
