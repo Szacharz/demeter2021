86c6a7953e685ef3844b8436fbddb58ddad9263c
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-27 10:44:19 +0200

    12

--- app/Http/Controllers/NewUserController.php
+++ app/Http/Controllers/NewUserController.php
@@ -52,21 +52,18 @@ class NewUserController extends Controller
     {
         return view('newuser');
     }
-    protected function validator(array $data)
+    public function store()
     {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        $this->validate(request(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
-    }
-
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        
+        $user = User::create(request(['name', 'email', 'password']));
+        
+        auth()->login($user);
+        
+        return redirect()->to('/games');
     }
 }
