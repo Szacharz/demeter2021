5dc27386756aaa9338f05bd1e7dd477a33bd05be
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-27 09:12:37 +0200

    Create newuser.blade.php

---
+++ resources/views/newuser.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.admin')
+
+
+<!-- Bootstrap library -->
+
+
+@section('content')
+<!-- Content Header (Page header) -->
+
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+              <!-- dodanie po prawej stronie w headzie, nie main headzie -->
+              <ol class="breadcrumb float-sm-right">
+          
+              
+          </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+<style>
+a
+{text-decoration: none;
+ background-color: none;
+ color:black; }
+
+ .cell-breakWord {
+  word-break: break-word;
+ }
+
+</style>
+
+
+
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+          <!-- right col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    @endsection
