0b517db8d1a9f137b505892ecc5e26524cc622ad
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-27 09:06:43 +0200

    Update manage.blade.php

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -90,7 +90,7 @@ a
             <h1><i class="fa fa-shield"></i> Zarządzanie użytkownikami </h1>
             <div class="form-group row">
             <a class="btn btn-info " href='http://dementor/reporthis'role="button"><h6><i class="fa fa-plus"></i> Nowy użytkownik</h6></a>
-            <div class="col-md" align="center">
+            <div class="col-lg" align="center">
           *  Do tej części strony dostęp mają tylko administratorzy serwisu Dementor.
               </div>  
                 </div>
