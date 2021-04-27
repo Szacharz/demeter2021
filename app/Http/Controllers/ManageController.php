d275f3390d14c4d626ddd2b6135333723ab1271b
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-27 12:14:15 +0200

    Update ManageController.php

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -34,8 +34,8 @@ class ManageController extends Controller
     }
     function edit3(Request $req)
     {
-        $users= users::find('id');
-        $users= users::where('id', $id)->update(array('role'=> $role));
+        $users= user::find('id');
+        $users= user::where('id', $id)->update(array('role'=> $role));
         $users->save();
         return view('manage')->with('success', 'Pomyślnie zmieniono role użytkownikowi!');
     }
