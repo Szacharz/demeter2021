d8db6788ddcfb319b12135b357d11226879fb45b
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-28 08:29:52 +0200

    12

--- resources/views/reporthis.blade.php
+++ resources/views/reporthis.blade.php
@@ -196,7 +196,7 @@ $(document).ready(function() {
             "targets": 0,
             "render": function(data, type, row) {return '<button class="btn btn-primary" data-toggle="modal" data-id="'+row.id+'" data-fieldname="'+row.fieldname+'" data-target="#myModal">'+data+'</button>'} 
         } ],
-        "lengthMenu": [[-1, 10, 25, 50], ["All", 10, 25, 50, ]],
+        "lengthMenu": [[-1, 10, 25, 50], ["Wszystkie", 10, 25, 50, ]],
         "order": [[ 1, 'asc' ]],
         
     } );
