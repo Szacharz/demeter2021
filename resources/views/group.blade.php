acbd9d421ed9a60af835a11037836b5a101a4d92
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-04-28 14:14:47 +0200

    Create group.blade.php

---
+++ resources/views/group.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+
+<!-- Bootstrap library -->
+
+<?php header('Refresh: 300'); ?>
+
+@section('content')
+
+
+    <!-- Main content -->
+    <section class="content">
+
+
+
+          <style>
+a
+{text-decoration: none;
+ background-color: none;
+ color:black; }
+
+ .cell-breakWord {
+  word-break: break-word;
+ }
+
+</style>          
+         
+
+
+  </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+          <!-- right col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+    @endsection
