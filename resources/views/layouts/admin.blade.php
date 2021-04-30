a89d7bb6e24f545c1367b68d822f4024133026c0
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 12:26:45 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -53,6 +53,7 @@
       </li>
       <div class="col-m-3">
   <a class="btn btn-info " href='http://dementor/home' role="button" aria-expanded="false"><h7> <i class="fa fa-home"></i> Strona główna</h7> </a>
+  </div>
 
   <div class="col-m-3 dropdown">
 <a class="btn btn-success dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h7><i class="fa fa-shield"></i> 
