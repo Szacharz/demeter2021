aa468df78d8cd032070e7d7a2f7b596c3bd3117a
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 12:16:14 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -56,8 +56,8 @@
   </div>
 
   <div class="col-m-3 dropdown">
-<a class="btn btn-success dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h6><i class="fa fa-shield"></i> 
-    Wpisy </h6>
+<a class="btn btn-success dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h7><i class="fa fa-shield"></i> 
+    Wpisy </h7>
   </a>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
   <a class="dropdown-item" href='http://dementor/payin' role="button"><h6> <i class="fa fa-plus-circle"></i> Nowy Wpis </h6></a>
