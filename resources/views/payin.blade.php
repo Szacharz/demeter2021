2302e4b4f4da053ac57e93ca5db4b683260a0be7
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 14:36:48 +0200

    Update payin.blade.php

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -197,7 +197,7 @@ FormStuff.init();
                     <option value="">DK-Darek&Kuba</option>
                     <option value="">DM-Dominik&Miłosz</option>
                     </select>
-              <p><h5>Tabela grup</h5></p>
+              <p><h7>Tabela dostępnych grup</h7></p>
              <table class="table table-striped table-bordered text-center table-hover table-responsive-lg" id="usterki"> 
               <thead>
                 <tr>
