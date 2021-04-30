98aac3e92d70e7409019ec6950caf1d7143ae92a
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 13:20:50 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -66,14 +66,14 @@
   
 
   <a class="btn btn-danger " href='http://dementor/expiration'role="button"><h7><i class="fa fa-gavel"></i> Przedawnione wpisy</h7></a>
-  <a class="btn btn-orange " href='http://dementor/payout'role="button"><h7><i class="fa fa-lock"></i> Lista prywatna</h7> </a>
- <a class="btn btn-warning " href='http://dementor/group'role="button"><h7><i class="fa fa-users"></i> Grupowe </h7></a>
- <a class="btn btn-secondary " href='http://dementor/reporthis'role="button"><h7><i class="fa fa-archive"></i> Archiwum </h7></a>
+  <a class="btn btn-pink " href='http://dementor/payout'role="button"><h7><i class="fa fa-lock"></i> Lista prywatna</h7> </a>
+ <a class="btn btn-orange " href='http://dementor/group'role="button"><h7><i class="fa fa-users"></i> Grupowe </h7></a>
+ <a class="btn btn-warning " href='http://dementor/reporthis'role="button"><h7><i class="fa fa-archive"></i> Archiwum </h7></a>
   
   </div>
 @if(Auth::user()->role== "Admin")
 <div class="col-m-3 dropdown">
-<a class="btn btn-pink dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-shield"></i> 
+<a class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-shield"></i> 
     Panel Administracyjny
   </a>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
