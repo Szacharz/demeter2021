a20c9b5ee78bfbe4616c2e3a896dec9fcbaaf062
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 11:33:17 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -61,7 +61,7 @@
   
 @if(Auth::user()->role== "Admin")
 <div class="dropdown">
-<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
     Panel Administracyjny
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
