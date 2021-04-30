1958b368863a618e88d5efb353f2849eed28515b
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 12:38:52 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -60,6 +60,7 @@
   <a class="btn btn-danger " href='http://dementor/expiration'role="button"><h7><i class="fa fa-gavel"></i> Przedawnione wpisy</h7></a>
   <a class="btn btn-warning " href='http://dementor/payout'role="button"><h7><i class="fa fa-lock"></i> Lista prywatna</h7> </a>
   <a class="btn btn-info " href='http://dementor/reporthis'role="button"><h7><i class="fa fa-archive"></i> Archiwum </h7></a>
+  </div>
 @if(Auth::user()->role== "Admin")
 <div class="col-m-3 dropdown">
 <a class="btn btn-info dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-shield"></i> 
