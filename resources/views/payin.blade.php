046a8d07195e6a2182eab515e47c5c988b3474fc
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 13:26:48 +0200

    Update payin.blade.php

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -172,7 +172,7 @@ FormStuff.init();
                    <option>Tak</option>
                  </select>
     </div>
-    <label for="form-check"> Projekt grupowy? [funkcja w rozwoju] </label>
+    <label for="form-check"> Projekt grupowy? [funkcja w budowie] </label>
     <div class="form-check form-check-inline">
     <input class="form-check-input" data-toggle="collapse" data-target="#collapseExample" aria-controls="collapseExample" aria-expanded="false" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Tak">
   <label class="form-check-label" for="inlineRadio1">Tak</label>
