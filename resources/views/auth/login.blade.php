bcd052ccdc8df21c04f1d18a628339a8b16d922b
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-04-30 10:15:42 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,9 +25,13 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adres Email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autofocus>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
 
-                
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </div>
 
