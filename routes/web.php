0ba6dd0eac506c35a3e7cdd9fb5ceaaa9dbfd543
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-10 14:30:29 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,14 @@ Route::get('/newuser', 'App\Http\Controllers\NewUserController@index');
 Route::get('/register', 'App\Http\Controllers\NewUserController@create');
 Route::post('register', 'App\Http\Controllers\NewUserController@store');
 
+/*Tworzenie grup*/
 Route::get('/group', 'App\Http\Controllers\GroupController@index');
+Route::get('/dictionary', 'App\Http\Controllers\DictionaryController@index');
+Route::get('/newgroup', 'App\Http\Controllers\NewGroupController@index');
+Route::post('/groupsubmit', 'App\Http\Controllers\NewGroupController@save');
 
+
+/*Zmiana Hasła i name*/
 Route::post('/changepassd', 'App\Http\Controllers\ProfileController@store');
 Route::post('/changenick', 'App\Http\Controllers\ProfileController@nick');
 
