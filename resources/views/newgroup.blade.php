742672178c09a5f247f428eea0580c03f9010ad3
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-10 13:08:38 +0200

    Create newgroup.blade.php

---
+++ resources/views/newgroup.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.admin')
+
+
+<!-- Bootstrap library -->
+
+
+@section('content')
+<!-- Content Header (Page header) -->
+    <!-- Main content -->
+    <section class="content">
+      <br /> <br />  
+
+          <script type="text/javascript">
+        
+         $('data').datepicker({
+       todayBtn: "linked",
+       language: "it",
+       autoclose: true,
+       todayHighlight: true,
+       Format: 'dd/mm/yyyy' 
+       });
+
+       function myFunction()
+       {
+        var x = document.getElementById("tresc").autofocus;
+       }
+});
+     }
+     var FormStuff = {
+  
+  init: function() {
+    // kick it off once, in case the radio is already checked when the page loads
+    this.applyConditionalRequired();
+    this.bindUIActions();
+  },
+  
+  bindUIActions: function() {
+    // when a radio or checkbox changes value, click or otherwise
+    $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
+  },
+  
+  applyConditionalRequired: function() {
+    // find each input that may be hidden or not
+    $(".require-if-active").each(function() {
+      var el = $(this);
+      // find the pairing radio or checkbox
+      if ($(el.data("require-pair")).is(":checked")) {
+        // if its checked, the field should be required
+        el.prop("required", true);
+      } else {
+        // otherwise it should not
+        el.prop("required", false);
+      }
+    });
+  }
+
+};
+
+FormStuff.init();
+</script>
+
+@if (session('success'))
+    <div class="col-sm-12">
+        <div class="alert  alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+        </div>
+    </div>
+@endif
+    
+<style>
+  .reveal-if-active {
+  opacity: 0;
+  max-height: 0;
+  overflow: hidden;
+  transform: scale(0.8);
+  transition: 0.5s;
+  input[type="radio"]:checked ~ &,
+  input[type="checkbox"]:checked ~ & {
+    opacity: 1;
+    max-height: 100px;
+    overflow: visible;
+    padding: 10px 20px;
+    transform: scale(1);
+  }
+}
+</style>
+
+
+
+
+   <div class="container">
+    <div class="row justify-content-center align-items-center">
+        <div class="col-10 col-md-8 col-lg-6">
+            <!-- Form -->
+            @if(count($errors)>0)
+           <ul>
+      @foreach($errors->all() as $error)
+    <li class="alert alert-danger">{{$error}}</li>
+    @endforeach
+    </ul>
+    @endif
+            <form class="form-example" action="/groupsubmit" method="POST">
+            @csrf
+                <h1><i class="fa fa-plus-circle"></i> Tworzenie grupy</h1>
+                <p class="description">Dodaj grupę, wypełniając forumlarz.</p>
+                <!-- Input fields -->
+                <div class="form-group">
+                    <label for="tresc">Grupa:</label>
+                    <textarea  height="100%" class="form-control" id="tresc" placeholder="Podaj nazwe grupy" name="group_desc" autofocus></textarea>
+                </div>
+      <div class="row">
+                <button type="submit" class="btn btn-default">Dodaj wpis</button>
+                <!-- End input fields -->
+                </div>
+                </form>
+                </div>
+            </form>
+            <!-- Form end -->
+        </div>
+    </div>
+</div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+          <!-- right col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+
+     
+    </section>
+    <!-- /.content -->
+    
+    @endsection
