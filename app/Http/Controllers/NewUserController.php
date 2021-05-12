df186d034787357bb1730c3d3b79adf0d53a1ebc
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-12 12:03:10 +0200

    Update NewUserController.php

--- app/Http/Controllers/NewUserController.php
+++ app/Http/Controllers/NewUserController.php
@@ -32,7 +32,7 @@ class NewUserController extends Controller
         $user = User::create([
             'name' => request('name'),
             'email' => request('email'),
-            'password' => bcrypt('password')
+            'password' => 'password'
              ]);
 
 
