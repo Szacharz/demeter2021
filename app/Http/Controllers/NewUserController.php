45accf08846f44e0c29980938e21d76f2fe1fde5
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-12 10:18:08 +0200

    Update NewUserController.php

--- app/Http/Controllers/NewUserController.php
+++ app/Http/Controllers/NewUserController.php
@@ -29,7 +29,7 @@ class NewUserController extends Controller
         $user = User::create([
             'name' => request('name'),
             'email' => request('email'),
-            'password' => request('password')
+            'password' => Hash::make('password')
              ]);
 
 
