f4b3a0b8cfad7bba348f0e19346aa4496d28e3e4
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-25 14:16:12 +0200

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -146,7 +146,10 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-        
+               
+          <?php
+            $segment = Request::segment(2);
+          ?>     
           <li class="nav-item has-treeview">
             <a href="{{ route('home') }}" class="nav-link 
               @if(!$segment)
