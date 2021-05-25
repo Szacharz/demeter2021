40be49bc06b738a7e94d14648b8a8d00f76646ad
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-25 12:08:11 +0200

    Update payin.blade.php

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -191,6 +191,7 @@ FormStuff.init();
                <label for="grupy" class="control-label col-sm-3 text-nowrap">Grupa: </label>
                     <div class="col-sm-9">
                     <select class="form-control" id="group_desc" name="group_desc">
+                    <option value=""></option>
                     @foreach($grupa as $row)
                     <option value="{{ $row['group_desc'] }}">{{ $row['group_desc'] }}</option>
                     @endforeach
