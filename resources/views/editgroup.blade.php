899f78668f8e6fbd3e2098431ad707d0ba3b9dd4
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-25 07:43:57 +0200

    Update editgroup.blade.php

--- resources/views/editgroup.blade.php
+++ resources/views/editgroup.blade.php
@@ -42,7 +42,7 @@
      
      @csrf
           <div class="form-group mb-2">
-           <div class="card-header cell-breakWord"><h1>Edytowany grupa: {{$grupa['group_desc']}}</h1> <h5><b></b></h5> </div>
+           <div class="card-header cell-breakWord"><h1>Edytowana grupa: {{$grupa['group_desc']}}</h1> <h5><b></b></h5> </div>
            </div>
            </div>
            <input type="hidden" name="id" id="id" value="{{$grupa['id']}}">
