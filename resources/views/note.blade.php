d1b5e547447f4acec3dcf1ee6b164db25abcc275
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-26 12:29:40 +0200

    Update note.blade.php

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -40,7 +40,7 @@
 <br>
 <div class="container">
         <div class="row justify-content-center align-items-center">
-        <div class="col-sm-6">
+        <div class="col-sm-8">
           <div class="card">
             <!-- Form -->
             @if(count($errors)>0)
