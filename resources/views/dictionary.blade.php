02cdcae3d6daa15b986fbf696d8b0a9c0ad79c4f
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-26 08:25:00 +0200

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -232,18 +232,18 @@ $(document).ready(function() {
 
 
 <div class="container-xl">
-            
             <div class="column"> <!-- przez to że jest zamknięta w kolumnie, jest mniejsza datatabela -->
-  
-  <div class="card">
-              <div class="card-header">
-              <div class="col-lg" align="center">
-            <h1><i class="fa fa-book"></i> Słownik - definiowanie grup do projektów grupowych </h1>
-            <div class="form-group row">
-            <a class="btn btn-info " href='http://dementor/newgroup'role="button"><h6><i class="fa fa-plus"></i> Dodaj grupę </h6></a>  
+                <div class="card">
+                 <div class="card-header">
+                    <div class="col-lg" align="center">
+                     <h1><i class="fa fa-book"></i> Słownik </h1>
+                     </div>
+                        Definiowanie grup do projektów grupowych
+                          <div class="form-group row">
+                            <a class="btn btn-info " href='http://dementor/newgroup'role="button"><h6><i class="fa fa-plus"></i> Dodaj grupę </h6></a>  
+                          </div>
+                    </div>
                 </div>
-              </div>
-              </div>
               <div class="card-body">
               <table class="table table-striped table-bordered text-center table-hover table-responsive-lg" id="groups"> 
      <thead>
