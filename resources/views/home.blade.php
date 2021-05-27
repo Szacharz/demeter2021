035c141c8556f4c0f65e1cbece33caa5c2c6fb44
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-05-27 13:19:25 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -253,7 +253,7 @@ a
 
 
 <div class="card-body">
-<table class="table table-striped table-bordered text-center table-hover table-responsive" cellspacing="0" style="width:auto"  id="later">
+<table class="table table-striped table-bordered text-center table-hover table-responsive" id="later">
 <thead class="thead-dark">
       <tr>
       <th scope="col">Id </th>
