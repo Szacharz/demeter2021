48cacf016dd3abe9729cc23f56fefa0c40d54b18
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-11 13:15:32 +0200

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -359,8 +359,11 @@ $(document).ready(function() {
         <td></td>
         <td style="width:85px"><a href={{"note/".$row['id_usterki']}}>{{$row['data']}}</td>
         <td class="cell-breakWord"><a href={{"note/".$row['id_usterki']}}>{{$row['tresc']}}</td>
-
+        @if($row['data'] <= $todayDate)
+        <td><a href={{"note/".$row['id_usterki']}}><c>{{$row['deadline']}}</c></td>
+        @else 
         <td><a href={{"note/".$row['id_usterki']}}>{{$row['deadline']}}</td>
+        @endif
         <td><a href={{"note/".$row['id_usterki']}}>{{$row['autor']}}</td>
         <td class="text-info"> {{$row['notki']}} </td>
         <td>
