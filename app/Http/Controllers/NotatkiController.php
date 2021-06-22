e5aff9ac095f13fd74de671de95032ee33cb2e7a
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-22 12:55:08 +0200

    idnot

--- app/Http/Controllers/NotatkiController.php
+++ app/Http/Controllers/NotatkiController.php
@@ -44,6 +44,8 @@ class NotatkiController extends Controller
     function editnote(Request $req)
     {
         $Notatki= Notatki::find($req->input('id_notatki'));
+        $Notatki->tresc_nt=$req->tresc_nt;
+        $Notatki->save();
         return redirect('/report')->with('success', 'Pomyślnie edytowano notatkę!');
     }
 }
