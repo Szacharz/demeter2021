1ffe3b6c4154b68f2f4dee8b37be2fb12f7701b2
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-23 08:19:21 +0200

    Update CalendarController.php

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -25,9 +25,11 @@ class CalendarController extends Controller
      */
     public function index()
     {
+        $department_id=Auth::user()->department_id;
         $usterki = usterkimodel::where('private', "0")
         ->where('status', "Niewykonane", "W trakcie") 
         ->whereNull('group_desc')
+        ->where('department_id', $department_id)
         ->get();
         return view('calendar',['usterki'=>$usterki]);
     }
