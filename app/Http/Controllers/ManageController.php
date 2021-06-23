db7b26e85686dda609a098eb697a2abed8a2f440
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-23 09:29:17 +0200

    Update ManageController.php

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth
 
 
 class ManageController extends Controller
@@ -21,9 +21,11 @@ class ManageController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
+    {   
+        $department_id=Auth::user()->department_id;
         $users = user::where('role', "Standard")
         ->orWhere('role', "Admin")  
+        ->where('department_id', $department_id)
         ->get();
         return view('manage', ['users'=>$users]);
     }
