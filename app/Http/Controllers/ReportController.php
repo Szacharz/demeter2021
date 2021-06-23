5f189972e9c37958b5ed4143cf69bb854713d52a
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-23 09:15:50 +0200

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -23,10 +23,11 @@ class ReportController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
+    {   $department_id=Auth::user()->department_id;
         $usterki = usterkimodel::where('private', "0")
         ->where('status', "Niewykonane", "W trakcie") 
         ->whereNull('group_desc')
+        ->where('department_id'. $department_id)
         ->get();
         return view('report',['usterki'=>$usterki]);
     }
