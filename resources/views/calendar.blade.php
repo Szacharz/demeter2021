671d0f4d91efcbdfdabf47a5f84eea8291c0a5ab
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-23 08:16:59 +0200

    Update calendar.blade.php

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,15 +1,94 @@
 @extends('layouts.admin')
 
+
+<!-- Bootstrap library -->
 <?php
 use Carbon\Carbon;
 $todayDate = Carbon::now()->format('Y-m-d');
 header('Refresh: 300'); ?>
-<!-- Bootstrap library -->
+
+@section('content')
 
 
 
-<!-- Content Header (Page header) -->
-<style>
+    <!-- Main content -->
+    <section class="content">
+
+      <!--Niżej są karty, gdybym potrzebował
+           <div class="container-fluid">
+        Small boxes (Stat box)
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            small box 
+            <div class="small-box bg-info">
+            <a href="payin">
+              <div class="inner">
+              <div class="container">
+                <h3>Dodawanie wpisu </h3>
+                <p> Dodaj nowe zgłoszenie  </p>
+              </div>
+              <div class="icon">
+              </div>
+              </div>
+              <a href="payin" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+        
+          <div class="col-lg-3 col-6">
+           
+            <div class="small-box bg-success">
+            <a href="report">
+              <div class="inner">
+              <div class="container">
+                <h3>Lista zgłoszeń <sup style="font-size: 20px"></sup></h3>
+                <p> Podgląd wszystkich zgłoszeń <p>
+              </div>
+              <div class="icon">
+                </div>
+              </div>
+              <a href="report" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+       
+          <div class="col-lg-3 col-6">
+         
+            <div class="small-box bg-warning">
+            <a href="payout">
+              <div class="inner">
+              <div class="container">
+                <h3>Lista Prywatna </h3>
+
+                <p>Podgląd prywatnych zadań </p>
+              </div>
+              <div class="icon">
+              </div>
+              </div>
+              <a href="payout" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+      
+         <div class="col-lg-3 col-6">
+         
+            <div class="small-box bg-danger">
+            <a href="reporthis">
+              <div class="inner">
+              <div class="container">
+                <h3>Archiwum</h3>
+
+                <p>Podgląd archiwalnych wpisów</p>
+              </div>
+              <div class="icon">
+              </div>
+              </div>
+              <a href="reporthis" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          -->
+
+
+
+
+          <style>
 a
 {text-decoration: none;
  background-color: none;
@@ -232,79 +311,19 @@ $(document).ready(function() {
 } );
 </script>
 
-@section('content')
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-            <a href="payin">
-              <div class="inner">
-              <div class="container">
-                <h3>Dodawanie wpisu </h3>
-                <p> Dodaj nowe zgłoszenie  </p>
-              </div>
-              <div class="icon">
-              </div>
-              </div>
-              <a href="payin" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-            <a href="report">
-              <div class="inner">
-              <div class="container">
-                <h3>Lista zgłoszeń <sup style="font-size: 20px"></sup></h3>
-                <p> Podgląd wszystkich zgłoszeń <p>
-              </div>
-              <div class="icon">
-                </div>
-              </div>
-              <a href="report" class="small-box-footer">Kliknij by przejść dalej <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-            <a href="payout">
-              <div class="inner">
-              <div class="container">
-                <h3>Lista Prywatna </h3>
+<br>
+@if (session('success'))
+    <div class="col-sm-12">
+        <div class="alert  alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+        </div>
+    </div>
+@endif
 
-                <p>Podgląd prywatnych zadań </p>
-              </div>
-              <div class="icon">
-              </div>
-              </div>
-              <a href="payout" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-         <!-- ./col -->
-         <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-            <a href="reporthis">
-              <div class="inner">
-              <div class="container">
-                <h3>Archiwum</h3>
-
-                <p>Podgląd archiwalnych wpisów</p>
-              </div>
-              <div class="icon">
-              </div>
-              </div>
-              <a href="reporthis" class="small-box-footer">Kliknij by przejść dalej<i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-
-          <div class="container-xl">
+<div class="container-xl">
     <div class="column"> <!-- przez to że jest zamknięta w kolumnie, jest mniejsza datatabela -->
          <div class="card">
             <div class="card-header">
@@ -387,184 +406,3 @@ $(document).ready(function() {
     </section>
     <!-- /.content -->
     @endsection
-<!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- jQuery UI -->
-<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../dist/js/adminlte.min.js"></script>
-<!-- fullCalendar 2.2.5 -->
-<script src="../plugins/moment/moment.min.js"></script>
-<script src="../plugins/fullcalendar/main.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-<!-- Page specific script -->
-<script>
-  $(function () {
-
-    /* initialize the external events
-     -----------------------------------------------------------------*/
-    function ini_events(ele) {
-      ele.each(function () {
-
-        // create an Event Object (https://fullcalendar.io/docs/event-object)
-        // it doesn't need to have a start or end
-        var eventObject = {
-          title: $.trim($(this).text()) // use the element's text as the event title
-        }
-
-        // store the Event Object in the DOM element so we can get to it later
-        $(this).data('eventObject', eventObject)
-
-        // make the event draggable using jQuery UI
-        $(this).draggable({
-          zIndex        : 1070,
-          revert        : true, // will cause the event to go back to its
-          revertDuration: 0  //  original position after the drag
-        })
-
-      })
-    }
-
-    ini_events($('#external-events div.external-event'))
-
-    /* initialize the calendar
-     -----------------------------------------------------------------*/
-    //Date for the calendar events (dummy data)
-    var date = new Date()
-    var d    = date.getDate(),
-        m    = date.getMonth(),
-        y    = date.getFullYear()
-
-    var Calendar = FullCalendar.Calendar;
-    var Draggable = FullCalendar.Draggable;
-
-    var containerEl = document.getElementById('external-events');
-    var checkbox = document.getElementById('drop-remove');
-    var calendarEl = document.getElementById('calendar');
-
-    // initialize the external events
-    // -----------------------------------------------------------------
-
-    new Draggable(containerEl, {
-      itemSelector: '.external-event',
-      eventData: function(eventEl) {
-        return {
-          title: eventEl.innerText,
-          backgroundColor: window.getComputedStyle( eventEl ,null).getPropertyValue('background-color'),
-          borderColor: window.getComputedStyle( eventEl ,null).getPropertyValue('background-color'),
-          textColor: window.getComputedStyle( eventEl ,null).getPropertyValue('color'),
-        };
-      }
-    });
-
-    var calendar = new Calendar(calendarEl, {
-      headerToolbar: {
-        left  : 'prev,next today',
-        center: 'title',
-        right : 'dayGridMonth,timeGridWeek,timeGridDay'
-      },
-      themeSystem: 'bootstrap',
-      //Random default events
-      events: [
-        {
-          title          : 'All Day Event',
-          start          : new Date(y, m, 1),
-          backgroundColor: '#f56954', //red
-          borderColor    : '#f56954', //red
-          allDay         : true
-        },
-        {
-          title          : 'Long Event',
-          start          : new Date(y, m, d - 5),
-          end            : new Date(y, m, d - 2),
-          backgroundColor: '#f39c12', //yellow
-          borderColor    : '#f39c12' //yellow
-        },
-        {
-          title          : 'Meeting',
-          start          : new Date(y, m, d, 10, 30),
-          allDay         : false,
-          backgroundColor: '#0073b7', //Blue
-          borderColor    : '#0073b7' //Blue
-        },
-        {
-          title          : 'Lunch',
-          start          : new Date(y, m, d, 12, 0),
-          end            : new Date(y, m, d, 14, 0),
-          allDay         : false,
-          backgroundColor: '#00c0ef', //Info (aqua)
-          borderColor    : '#00c0ef' //Info (aqua)
-        },
-        {
-          title          : 'Birthday Party',
-          start          : new Date(y, m, d + 1, 19, 0),
-          end            : new Date(y, m, d + 1, 22, 30),
-          allDay         : false,
-          backgroundColor: '#00a65a', //Success (green)
-          borderColor    : '#00a65a' //Success (green)
-        },
-        {
-          title          : 'Click for Google',
-          start          : new Date(y, m, 28),
-          end            : new Date(y, m, 29),
-          url            : 'https://www.google.com/',
-          backgroundColor: '#3c8dbc', //Primary (light-blue)
-          borderColor    : '#3c8dbc' //Primary (light-blue)
-        }
-      ],
-      editable  : true,
-      droppable : true, // this allows things to be dropped onto the calendar !!!
-      drop      : function(info) {
-        // is the "remove after drop" checkbox checked?
-        if (checkbox.checked) {
-          // if so, remove the element from the "Draggable Events" list
-          info.draggedEl.parentNode.removeChild(info.draggedEl);
-        }
-      }
-    });
-
-    calendar.render();
-    // $('#calendar').fullCalendar()
-
-    /* ADDING EVENTS */
-    var currColor = '#3c8dbc' //Red by default
-    // Color chooser button
-    $('#color-chooser > li > a').click(function (e) {
-      e.preventDefault()
-      // Save color
-      currColor = $(this).css('color')
-      // Add color effect to button
-      $('#add-new-event').css({
-        'background-color': currColor,
-        'border-color'    : currColor
-      })
-    })
-    $('#add-new-event').click(function (e) {
-      e.preventDefault()
-      // Get value and make sure it is not null
-      var val = $('#new-event').val()
-      if (val.length == 0) {
-        return
-      }
-
-      // Create events
-      var event = $('<div />')
-      event.css({
-        'background-color': currColor,
-        'border-color'    : currColor,
-        'color'           : '#fff'
-      }).addClass('external-event')
-      event.text(val)
-      $('#external-events').prepend(event)
-
-      // Add draggable funtionality
-      ini_events(event)
-
-      // Remove event from text input
-      $('#new-event').val('')
-    })
-  })
-</script>
