a1b360c14dd3c7ddffd49be44f1480fd232c3e88
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-23 10:05:41 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -163,6 +163,14 @@ $("#datapozniej").blur();
   </div>
 
 
+  <div class="card text-white bg-danger">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Danger card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
 <br> </br>
 @endif
             
