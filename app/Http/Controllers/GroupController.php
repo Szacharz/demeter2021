16b7cd6983dd1145cb9b4c164d167d0f9ab2cff1
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-23 08:58:48 +0200

    Update GroupController.php

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\usterkimodel;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 
 class GroupController extends Controller
@@ -23,8 +23,10 @@ class GroupController extends Controller
      */
     public function index()
     { 
+        $department_id=Auth::user()->department_id;
         $usterki = usterkimodel::where('status', "Niewykonane")
         ->whereNotNull('group_desc')
+        ->where('department_id', $department_id)
         ->get();
         return view('group', ['usterki'=>$usterki]);
     }
