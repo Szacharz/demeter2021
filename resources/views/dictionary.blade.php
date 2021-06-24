c42ee68e69674d67456fe7caa97012e0e9919288
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-24 13:32:37 +0200

    Update dictionary.blade.php

--- resources/views/dictionary.blade.php
+++ resources/views/dictionary.blade.php
@@ -268,11 +268,12 @@ $(document).ready(function() {
         <td></td>
         <td>{{$row['group_desc']}}</td>
 
-        @for ($row['id'] == $membersi['id']) 
+        @for ($row['id'] == $membersi['id'];) 
         @foreach($membersi as $member)  
         <td style="width:100px">{{$member['name']}}</td>
         @endforeach
          @endfor
+
         <td><a href={{"editgroup/".$row['id']}} class="btn btn-warining" role="button"><h6><i class="fa fa-pencil"></i></h6></a>
       </tr>
       @endforeach
