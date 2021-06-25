3b92afa4666d3b1b6818fc2fed090d39cdb3d8a6
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-25 14:08:51 +0200

    Update NewGroupController.php

--- app/Http/Controllers/NewGroupController.php
+++ app/Http/Controllers/NewGroupController.php
@@ -31,7 +31,7 @@ class NewGroupController extends Controller
 	$groups->save();
 
     $member=$req->member;
-    foreach($GroupMembers=$req->member as $member) {
+    foreach($member as $member) {
     $GroupMembers = new GroupMembers;
     $GroupMembers->group_id=$groups->id;
     $GroupMembers->user_id=$req->member;
