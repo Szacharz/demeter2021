836ce8c0ea2479b938468fed7cc2a03a3c23c6cc
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-06-25 11:49:32 +0200

    Update note.blade.php

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -246,14 +246,21 @@ $(document).ready(function() {
 </style>
 
 
-           @if(Auth::user()->department_id == $usterki['department_id'])
+           
 <br>
 <div class="container">
         <div class="row justify-content-center align-items-center">
         <div class="col-sm-8">
           <div class="card">
             <!-- Form -->
-           
+            @if(count($errors)>0)
+           <ul>
+      @foreach($errors->all() as $error)
+    <li class="alert alert-danger">{{$error}}</li>
+    @endforeach
+    </ul>
+    @endif
+    @if(Auth::user()->department_id == $usterki['department_id'])
            <form class="form-example" action="/notesubmit" method="POST">
             @csrf
             <input type="hidden" name="id_usterki" id="id_usterki" value="{{$usterki['id_usterki']}}">
