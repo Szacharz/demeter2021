4e5a98a71b6a12671c9ae3cd5d4ab19790424291
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-05 08:44:39 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -67,9 +67,11 @@ margin-bottom: auto;
                <div class="card-header"> <h1>Edycja Wpisu o ID: {{$usterki['id_usterki']}}</h1>
                 <p class="description">Dodaj nowy Wpis, wypełniajac formularz.</p>
               </div>
-              <div class="text-red">1. Opis wpisu: {{$usterki['tresc']}} </div>
-                <div class="text-red">2. Status wpisu: {{$usterki['status']}} </div>
-                <div class="text-red">3. Deadline: {{$usterki['deadline']}} </div>
+              <div class="p-3 mb-2 bg-dark text-white">
+              <div class="text-info">1. Opis wpisu: {{$usterki['tresc']}} </div>
+                <div class="text-info">2. Status wpisu: {{$usterki['status']}} </div>
+                <div class="text-info">3. Deadline: {{$usterki['deadline']}} </div>
+                </div>
               </div>
               <!-- Input fields -->
                   <div class="card-body">
