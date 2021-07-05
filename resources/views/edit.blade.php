442d8507f65a4264e638786df1732cb276c132b0
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-05 08:21:14 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -66,7 +66,7 @@ margin-bottom: auto;
            <div class="p-3 mb-2 bg-dark text-white"> <!-- Form -->
                <div class="card-header"> <h1>Edycja Wpisu o ID: {{$usterki['id_usterki']}}</h1>
                 <p class="description">Dodaj nowy Wpis, wypełniajac formularz.</p>
-                <div class="text-red">Pamiętaj! Jeśli wpis ma pozostać prywatny zaznacz, że wpis jest prywatny. </div>
+                <div class="text-red"> Status: {{$usterki['status']}} Wpis prywatny: {{$usterki['private']}}   </div>
               </div>
               </div>
               <!-- Input fields -->
