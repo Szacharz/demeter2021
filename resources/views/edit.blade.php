8de491fa5d44739bc1af8ef21fadea95cbef91ea
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-05 09:34:41 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -72,8 +72,8 @@ margin-bottom: auto;
                   <div class="card-body">
                   <div class="form-group">
                   Dane wpisu:
-              <div class="text-info">1. Opis wpisu: {{$usterki['tresc']}} 2. Data zakończenia prac: {{$usterki['deadline']}}  </div>
-                <div class="text-info">3. Status wpisu: {{$usterki['status']}} @if($usterki['private'] == 1) 4. Prywatny: Tak  @else 4. Prywatny: Nie @endif</div>
+              <div class="text-info">1. Opis wpisu: {{$usterki['tresc']}}     2. Data zakończenia prac: {{$usterki['deadline']}}  </div>
+                <div class="text-info">3. Status wpisu: {{$usterki['status']}} @if($usterki['private'] == 1)  4. Prywatny: Tak  @else 4. Prywatny: Nie @endif</div>
                    <div class="form-group">
                     <label for="tresc">Opis wpisu:</label>
                     <input type="text" class="form-control" id="tresc" placeholder="Podaj treść wpisu..." name="tresc" value="{{$usterki['tresc']}}">
