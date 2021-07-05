526bd3052e03b9d16b0e3e040f3051787aac7b75
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-05 09:36:08 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -70,8 +70,8 @@ margin-bottom: auto;
               </div>
               <!-- Input fields -->
                   <div class="card-body">
-                  <div class="form-group">
-                  Dane wpisu:
+                  <div class="form-group text-info">
+                  Informacje o wpisie:
               <div class="text-info">1. Opis wpisu: {{$usterki['tresc']}}     2. Data zakończenia prac: {{$usterki['deadline']}}  </div>
                 <div class="text-info">3. Status wpisu: {{$usterki['status']}} @if($usterki['private'] == 1)  4. Prywatny: Tak  @else 4. Prywatny: Nie @endif</div>
                    <div class="form-group">
