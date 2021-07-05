fd6ada64b83070c3002fd710b350e4b90b213248
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-05 08:35:25 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -81,7 +81,7 @@ margin-bottom: auto;
                     <label for="data">Data wpisu:</label>
                     <input type="date" class="form-control" id="data"  name="data"  value="{{$usterki['data']}}" >
                 </div> -->
-
+                <input type="hidden" class="form-control" id="data"  name="data"  value="{{$usterki['data']}}"
                 <div class="form-group">
                 <label for="deadline">Deadline</label>
                     <select class="form-control" name="deadline" id="deadline" onChange="showDPNew(this)" >
