6c5fa77c1fc0e650fad44dd3178b19e311737277
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-07 09:17:46 +0200

    dict

--- app/Http/Controllers/DictionaryController.php
+++ app/Http/Controllers/DictionaryController.php
@@ -6,6 +6,7 @@ use App\Models\GroupMembers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Models\Departments;
 
 class DictionaryController extends Controller
 {
@@ -23,6 +24,10 @@ class DictionaryController extends Controller
      */
     public function index()
     {  
+        $department_id=Auth::user()->department_id;
+        $Departments = new Departments;
+        $Departments = Departments::where('id', $department_id)
+        ->get();
          $department_id=Auth::user()->department_id;
          $groups = groups::all();
          $grupy=DB::table('grupy')
@@ -38,7 +43,7 @@ class DictionaryController extends Controller
          });
          $array = $grupy->toArray();
 
-        return view('dictionary', ['grupy'=>$grupy]);
+        return view('dictionary', ['grupy'=>$grupy, 'departments'=>$Departments]);
     }
    
 }
