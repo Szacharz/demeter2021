26ab756495e0feb60173305cd594d71ee8c0c822
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-07-08 13:41:28 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::get('/newprivate', 'App\Http\Controllers\NewPrivateController@index')->na
 
 Route::get('/listdepartments', 'App\Http\Controllers\ListDepartmentsController@index')->name('listdepartments');
 
-Route::get('/newdepartment', 'App\Http\Controllers\NewDepartment@index')->name('newdepartment');
+Route::get('/newdepartment', 'App\Http\Controllers\NewDepartmentController@index')->name('newdepartment');
 
 /*Tworzenie użytkowników */
 Route::get('/profile', 'App\Http\Controllers\ProfileController@index');
