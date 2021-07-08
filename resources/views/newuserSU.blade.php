fb9c0bb27fbbaec039f6ef91664b4c7414ad74da
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-08 08:54:54 +0200

    Update newuserSU.blade.php

--- resources/views/newuserSU.blade.php
+++ resources/views/newuserSU.blade.php
@@ -125,7 +125,7 @@ a
             <div class="col-lg my-auto">
 
   <div class="card text-white bg-danger">
-  <div class="card-header"><h1><i class="fa fa-shield"></i> Edycja Wpisu </h1></div>
+  <div class="card-header"><h1><i class="fa fa-shield"></i> Nie wystarczające uprawnienia. </h1></div>
   <div class="card-body">
     <h5 class="card-title" align="center"> Brak Dostępu. </h5>
     <p class="card-text">  Do tej części strony dostęp ma tylko użytkownik należący do odpowiedniego działu. </p>
