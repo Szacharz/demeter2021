2ede5a698fd48d0e70d7656b4f3bd339c80ec58b
Szacharz <74829606+Szacharz@users.noreply.github.com>
2021-07-08 13:53:12 +0200

    Update newdepartment.blade.php

--- resources/views/newdepartment.blade.php
+++ resources/views/newdepartment.blade.php
@@ -37,67 +37,6 @@
 
 </style>
 
-<script type="text/javascript">
-    $(document).ready(function(){      
-      var postURL = "<?php echo url('addmore'); ?>";
-      var i=1;  
-
-
-      $('#add').click(function(){  
-           i++;  
-           $('#dynamic_field').append('<tr id="row'+i+'" class="dynamic-added"> <td><select class="form-control" name="member[]" id="member[]"><option value="" selected disabled>Wybierz użytkownika</option> @foreach($users as $item)<option value="{{$item->id}}"> {{$item->name}}</option> @endforeach </select></td> <td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
-      });  
-
-
-      $(document).on('click', '.btn_remove', function(){  
-           var button_id = $(this).attr("id");   
-           $('#row'+button_id+'').remove();  
-      });  
-
-
-      $.ajaxSetup({
-          headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-          }
-      });
-
-
-      $('#submit').click(function(){            
-           $.ajax({  
-                url:postURL,  
-                method:"POST",  
-                data:$('#add_name').serialize(),
-                type:'json',
-                success:function(data)  
-                {
-                    if(data.error){
-                        printErrorMsg(data.error);
-                    }else{
-                        i=1;
-                        $('.dynamic-added').remove();
-                        $('#add_name')[0].reset();
-                        $(".print-success-msg").find("ul").html('');
-                        $(".print-success-msg").css('display','block');
-                        $(".print-error-msg").css('display','none');
-                        $(".print-success-msg").find("ul").append('<li>Record Inserted Successfully.</li>');
-                    }
-                }  
-           });  
-      });  
-
-
-      function printErrorMsg (msg) {
-         $(".print-error-msg").find("ul").html('');
-         $(".print-error-msg").css('display','block');
-         $(".print-success-msg").css('display','none');
-         $.each( msg, function( key, value ) {
-            $(".print-error-msg").find("ul").append('<li>'+value+'</li>');
-         });
-      }
-    });  
-</script>
-
-
 <div class="container">
     <div class="row justify-content-center align-items-center">
         <div class="col-10 col-md-8 col-lg-6">
