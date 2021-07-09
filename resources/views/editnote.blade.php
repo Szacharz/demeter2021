65f9d7563cb10e6a7894d7f959cd30e09f3a7cec
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-09 07:21:47 +0200

    Update editnote.blade.php

--- resources/views/editnote.blade.php
+++ resources/views/editnote.blade.php
@@ -21,8 +21,9 @@
             @csrf
             <input type="hidden" name="id_notatki" id="id_notatki" value="{{$Notatki['id_notatki']}}">
          <br>
-         <div class="p-3 mb-2 bg-dark text-white">
-               <div class="card-header"> <h1>Edytuj notatkę</h1>
+      
+               <div class="card-header">   <div class="p-3 mb-2 bg-dark text-white"> 
+               <h1>Edytuj notatkę</h1>
                 <p class="description">Edytuj istniejącą notatkę.</p>
                </div><!-- Input fields -->
                   <div class="card-body">
