c7edb601d3922a3e76bdc9d3d38bcd13c2388c09
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-09 08:54:55 +0200

    Update EditGroupController.php

--- app/Http/Controllers/EditGroupController.php
+++ app/Http/Controllers/EditGroupController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Crypt;
 
 class EditGroupController extends Controller
 {
