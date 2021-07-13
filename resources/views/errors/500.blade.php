a89b8507f1f9ff565556d89fcde4359bf8bcd36f
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-13 14:14:13 +0200

    132

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
 @section('title', __('Server Error'))
-@section('code', '500')
+@section('code', '500', '<a href="dementor/">Wróć na stronę logowania<a>')
 @section('message', __('Server Error'))
