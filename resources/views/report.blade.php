a0aea93d0c2330f409afdbcb4c6a5a95d1225bed
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-14 07:36:10 +0200

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -319,6 +319,15 @@ $(document).ready(function() {
         } );
     } ).draw();
 } );
+
+$(document).ready(function () {
+ 
+ window.setTimeout(function() {
+     $(".alert").fadeTo(500, 0).slideUp(500, function(){
+         $(this).remove(); 
+     });
+ }, 1500);
+ });
 </script>
 
 <br>
