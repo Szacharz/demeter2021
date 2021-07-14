a7d4b6d98c25dea39b95f734bdbdbbeef48d2961
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-14 09:49:00 +0200

    depastats

--- app/Http/Controllers/DepartmentStatsController.php
+++ app/Http/Controllers/DepartmentStatsController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use DB;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DepartmentStatsController extends Controller
 {
     public function index()
     {   
-       
+        $year = ['2018','2019','2020'];
+
+        $user = [];
+        foreach ($year as $key => $value) {
+            $user[] = User::where(\DB::raw("DATE_FORMAT(created_at, '%Y')"),$value)->count();
+        }
         return view('departmentstats');
     }
 }
