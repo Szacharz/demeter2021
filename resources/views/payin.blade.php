447d66730118fe29ac1f096bc97e86a63fabba06
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 10:13:53 +0200

    Update payin.blade.php

--- resources/views/payin.blade.php
+++ resources/views/payin.blade.php
@@ -250,7 +250,7 @@ $("#datapozniej").blur();
  
                 <!-- Input fields -->
                 <div class="form-group">
-                    <label for="tresc_nt"><u>Opcjonalnie</u> możesz dodać notatkę do wpisu.(jeszcze nie działa)</label>
+                    <label for="tresc_nt"><u>Opcjonalnie</u> możesz utworzyć notatkę do wpisu.(jeszcze nie działa)</label>
                     <textarea height="100%" class="form-control" name="tresc_nt" id="tresc_nt" placeholder="Wprowadź tekst notatki"></textarea>
                 </div>
                 <input type="hidden" name="autor" id="autor" value="{{Auth::user()->name }}">
