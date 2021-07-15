a1c35ddcd96304ec43ecf6547da8a3e501b4ceb3
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 14:21:17 +0200

    Update departmentstats.blade.php

--- resources/views/departmentstats.blade.php
+++ resources/views/departmentstats.blade.php
@@ -18,6 +18,17 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-10 offset-md-1">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <canvas id="canvas" height="280" width="600"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
 <script>
