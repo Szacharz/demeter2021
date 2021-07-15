acb945f07242efa1c4d2eb236e31e29f6af75362
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 07:41:25 +0200

    Update GroupController.php

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Departments;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class GroupController extends Controller
 {
