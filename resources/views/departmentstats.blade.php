d5893cfe95133d25803dc2c8be4dbfc2be049be1
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 12:30:34 +0200

    Update departmentstats.blade.php

--- resources/views/departmentstats.blade.php
+++ resources/views/departmentstats.blade.php
@@ -26,8 +26,8 @@
     var barChartData = {
         labels: month,
         datasets: [{
-            label: 'Usterki',
-            backgroundColor: "success",
+            label: 'Wpisy',
+            backgroundColor: "lightgreen",
             data: usterki
         }]
     };
