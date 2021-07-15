e8ab32e28f1ceff746fb74846dbda13162cfc0cd
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 14:47:46 +0200

    Update departmentstats.blade.php

--- resources/views/departmentstats.blade.php
+++ resources/views/departmentstats.blade.php
@@ -17,14 +17,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="col-md-10 offset-md-1">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <canvas id="canvas2" height="280" width="600"></canvas>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
@@ -50,28 +42,6 @@
     window.onload = function() {
         var ctx = document.getElementById("canvas").getContext("2d");
         window.myBar = new Chart(ctx, {
-            type: 'bar',
-            data: barChartData,
-            options: {
-                elements: {
-                    rectangle: {
-                        borderWidth: 2,
-                        borderColor: '#c1c1c1',
-                        borderSkipped: 'bottom'
-                    }
-                },
-                responsive: true,
-                title: {
-                    display: true,
-                    text: 'Ilość wpisów w danym miesiącu'
-                }
-            }
-        });
-        
-    };
-    window.onload = function() {
-        var ctx2 = document.getElementById("canvas2").getContext("2d");
-        window.myBar = new Chart(ctx2, {
             type: 'bar',
             data: barChartData,
             options: {
