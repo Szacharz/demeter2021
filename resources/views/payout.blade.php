138a79f271ff3f397472ef3f3a24e5065ab9855d
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 07:29:44 +0200

    Update payout.blade.php

--- resources/views/payout.blade.php
+++ resources/views/payout.blade.php
@@ -224,6 +224,14 @@ $(document).ready(function() {
         } );
     } ).draw();
 } );
+$(document).ready(function () {
+ 
+ window.setTimeout(function() {
+     $(".alert").fadeTo(500, 0).slideUp(500, function(){
+         $(this).remove(); 
+     });
+ }, 1500);
+ });
 </script>
 
 <style>
