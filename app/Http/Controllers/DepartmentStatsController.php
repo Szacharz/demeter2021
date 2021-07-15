0ab221a3385f7b2e9a0ed1d456ed42a4673bba6b
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 12:01:54 +0200

    Update DepartmentStatsController.php

--- app/Http/Controllers/DepartmentStatsController.php
+++ app/Http/Controllers/DepartmentStatsController.php
@@ -12,7 +12,7 @@ class DepartmentStatsController extends Controller
     {   
         $year = ['2019','2020', '2021'];
         $month = ['Styczeń','Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
-
+        $month = ['01','02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
         $user = [];
         foreach ($month as $key => $value) {
             $user[] = User::where(DB::raw("DATE_FORMAT(created_at, '%m')"),$value)->count();
