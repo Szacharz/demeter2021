d6720dda4cc95832562b40086dfea0526979bdbe
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-15 14:22:43 +0200

    Update departmentstats.blade.php

--- resources/views/departmentstats.blade.php
+++ resources/views/departmentstats.blade.php
@@ -23,7 +23,7 @@
         <div class="col-md-10 offset-md-1">
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <canvas id="canvas" height="280" width="600"></canvas>
+                    <canvas id="canvas2" height="280" width="600"></canvas>
                 </div>
             </div>
         </div>
@@ -52,6 +52,46 @@
 
     window.onload = function() {
         var ctx = document.getElementById("canvas").getContext("2d");
+        window.myBar = new Chart(ctx, {
+            type: 'bar',
+            data: barChartData,
+            options: {
+                elements: {
+                    rectangle: {
+                        borderWidth: 2,
+                        borderColor: '#c1c1c1',
+                        borderSkipped: 'bottom'
+                    }
+                },
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'Ilość wpisów w danym miesiącu'
+                }
+            }
+        });
+    };
+
+    var month = <?php echo $month; ?>;
+    var usterki = <?php echo $usterki; ?>;
+    var usterkifinished = <?php echo $usterkifinished; ?>;
+    var barChartData = {
+        labels: month,
+        datasets: [{
+            label: 'Wpisy',
+            backgroundColor: "lightgreen",
+            data: usterki
+        },
+            {
+            label: 'Ukończone Wpisy',
+            backgroundColor: "lightyellow",
+            data: usterkifinished
+            }
+    ]
+    };
+
+    window.onload = function() {
+        var ctx = document.getElementById("canvas2").getContext("2d");
         window.myBar = new Chart(ctx, {
             type: 'bar',
             data: barChartData,
