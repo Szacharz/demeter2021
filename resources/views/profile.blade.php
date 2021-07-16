c621631857f6894da2de599743adcf63c0605ada
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-16 13:29:08 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -52,8 +52,6 @@
            
    <div class="row">
    <div class="row justify-content-center align-items-center">
-     <h2> Panel użytkownika  </h2>
-</div>
             <div class="col-lg-6 mb-4">
             <!-- Form -->
             @if(count($errors)>0)
@@ -64,6 +62,7 @@
     </ul>
     @endif
     <div class="form-group mb-4">
+           
             <div class="card">
             
             <form class="form-example" action="/changepassd" method="POST">
