3ef1785265ef1e66a83173285a7b0304bf22b1f7
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-16 13:33:49 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -131,7 +131,7 @@
                                 <input id="name"  class="form-control" name="name">
                             </div>
                         </div>
-                  <br><br><br>
+                  <br><br>
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
