4ffc0267007fbf62318ad355ac908e751f5967af
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-19 14:22:15 +0200

    Update report.blade.php

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -128,6 +128,13 @@ a
           <script>
 $(document).ready(function() {
     var t = $('#usterki').DataTable( {
+      stateSave: true,
+        stateSaveCallback: function(settings,data) {
+      localStorage.setItem( 'DataTables_' + settings.sInstance, JSON.stringify(data) )
+    },
+  stateLoadCallback: function(settings) {
+    return JSON.parse( localStorage.getItem( 'DataTables_' + settings.sInstance ) )
+    },
       "language":{
     "processing": "Przetwarzanie...",
     "search": "Znajdź:",
