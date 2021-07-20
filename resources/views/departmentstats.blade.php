a02e1c794bb2b1c0ca260097617998234fd895fe
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-20 14:09:41 +0200

    Update departmentstats.blade.php

--- resources/views/departmentstats.blade.php
+++ resources/views/departmentstats.blade.php
@@ -22,7 +22,7 @@
 
 
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.4.1/Chart.min.js"></script>
 <script>
     var month = <?php echo $month; ?>;
     var usterki = <?php echo $usterki; ?>;
