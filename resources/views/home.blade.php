b9422d84cb96b1f736822cb88c3f4b427143c6ff
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-20 09:41:28 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -321,7 +321,11 @@ a
      
      @if($row['importance'] == '0' and $row['group_desc'] === NULL)
       <tr>
+    @if($row['private'] == '0')
         <td></td>
+        @else
+      <td class="td-yes"></td>
+      @endif
         <td style="width:85px"><a href={{"note/".$row['id_usterki']}}>{{$row['data']}}</td>
         <td class="cell-breakWord"><a href={{"note/".$row['id_usterki']}}>{{$row['tresc']}}</td>
         <td><a href={{"note/".$row['id_usterki']}}>{{$row['deadline']}}</td>
@@ -330,7 +334,11 @@ a
       </tr>
       @elseif($row['group_desc'] === NULL)
       <tr>
+      @if($row['private'] == '0')
        <b><td></td></b>
+       @else
+       <b><td class="td-yes"></td></b>
+      @endif
         <td style="width:85px"><a href={{"note/".$row['id_usterki']}}><b>{{$row['data']}}</b></td>
         <td class="cell-breakWord"><a href={{"note/".$row['id_usterki']}}><b>{{$row['tresc']}}</b></td>
         <td><a href={{"note/".$row['id_usterki']}}><b>{{$row['deadline']}}</b></td>
