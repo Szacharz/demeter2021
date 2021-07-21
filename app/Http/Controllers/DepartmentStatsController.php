afea33a99cceda382d796134aaea11afe1ac295c
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-21 07:56:31 +0200

    Update DepartmentStatsController.php

--- app/Http/Controllers/DepartmentStatsController.php
+++ app/Http/Controllers/DepartmentStatsController.php
@@ -43,7 +43,7 @@ class DepartmentStatsController extends Controller
 
         foreach($users as $key =>$value)
         {
-            $entryforuser[] = usterkimodel::where(DB::raw('autor'),$value)->count();
+            $entryforuser[] = usterkimodel::where(DB::raw("TEXT(autor)"),$value)->count();
         }
 
         return view('departmentstats')->with('month',json_encode($month,JSON_NUMERIC_CHECK))->with('user',json_encode($user,JSON_NUMERIC_CHECK))->with('usterki',json_encode($usterki,JSON_NUMERIC_CHECK))->with('usterkifinished',json_encode($usterkifinished,JSON_NUMERIC_CHECK))->with('monthsname')->with('entryforuser',json_encode($entryforuser,JSON_NUMERIC_CHECK))->with('users');
