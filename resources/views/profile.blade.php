faef6613503cc599ec57627922aff62f96a8829b
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-23 10:32:56 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -147,7 +147,7 @@
 
                     </div>
               </div>
-            </div>
+            
             <div class="row">
         <div class="col-md-10 offset-md-1">
             <div class="panel panel-default">
