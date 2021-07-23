2e51de2a2d71a99f1bbdaf40b344655fec526434
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-23 11:28:09 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -159,7 +159,7 @@
                 </div>
             </div>
             <div class="card-body">
-                <h3>Dobra robota! Wygląda na to, że wpisy są wykonywane przez Ciebie sumiennie. Tak trzymaj.</h3>
+                <h5>Dobra robota! Wygląda na to, że wpisy są wykonywane przez Ciebie sumiennie. Tak trzymaj.</h5>
         <div class="row">
            <div class="col-md-10 offset-md-1">
              <div class="panel panel-default">
