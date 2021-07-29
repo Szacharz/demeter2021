607b31879e9ea80a42c862cd4f3781bcbe0ee2a6
Miłosz Walkowski <m.walkowski@EUROCOMFORT.LOCAL>
2021-07-29 07:28:15 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Dementor</title>
-    <link rel = "icon" img src="img/D.png"  type = "image/x-icon">
+    <link rel = "icon" href="img/D.png"  type = "image/x-icon">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
